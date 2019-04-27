9d995dc224627fbdb1741dab308df9907087a1c7
Thomas Allmer <d4kmor@gmail.com>
2019-04-27 15:17:39 +0200

    fix: add missing exports to core, input-iban, localize

--- packages/input-iban/index.js
+++ packages/input-iban/index.js
@@ -1,1 +1,9 @@
 export { LionInputIban } from './src/LionInputIban.js';
+export { formatIBAN } from './src/formatters.js';
+export { parseIBAN } from './src/parsers.js';
+export {
+  isCountryIBAN,
+  isCountryIBANValidator,
+  isIBAN,
+  isIBANValidator,
+} from './src/validators.js';
