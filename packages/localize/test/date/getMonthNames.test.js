c9cf82aff8d70ef88fbce46b3ec4bd960a30657a
Thijs Louisse <Thijs.Louisse@ing.com>
2019-05-15 17:00:37 +0200

    feat(localize): allow long/short/narrow param getMonthNames

--- packages/localize/test/date/getMonthNames.test.js
+++ packages/localize/test/date/getMonthNames.test.js
@@ -18,4 +18,15 @@ describe('getMonthNames', () => {
       s`一月 二月 三月 四月 五月 六月 七月 八月 九月 十月 十一月 十二月`,
     );
   });
+  it('supports "short" style', () => {
+    expect(getMonthNames({ locale: 'en-GB', style: 'short' })).to.deep.equal(
+      s`Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec`,
+    );
+    expect(getMonthNames({ locale: 'nl-NL', style: 'short' })).to.deep.equal(
+      s`jan. feb. mrt. apr. mei jun. jul. aug. sep. okt. nov. dec.`,
+    );
+    expect(getMonthNames({ locale: 'zh-CH', style: 'short' })).to.deep.equal(
+      s`1月 2月 3月 4月 5月 6月 7月 8月 9月 10月 11月 12月`,
+    );
+  });
 });
