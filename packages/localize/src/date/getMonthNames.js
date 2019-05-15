58fefc4cfb430c0772f1f4c54b2f5e1eed56f3d0
Thijs Louisse <Thijs.Louisse@ing.com>
2019-05-15 17:00:37 +0200

    feat(localize): allow long/short/narrow param getMonthNames

--- packages/localize/src/date/getMonthNames.js
+++ packages/localize/src/date/getMonthNames.js
@@ -2,8 +2,14 @@ import { normalizeDate } from './normalizeDate.js';
 
 const monthsLocaleCache = {};
 
-export function getMonthNames({ locale }) {
-  let months = monthsLocaleCache[locale];
+/**
+ * @desc Returns month names for locale
+ * @param {string} options.locale locale
+ * @param {string} [options.style=long] long, short or narrow
+ * @returns {Array} like: ['Januray', 'February', ...etc].
+ */
+export function getMonthNames({ locale, style = 'long' } = {}) {
+  let months = monthsLocaleCache[locale] && monthsLocaleCache[locale][style];
 
   if (months) {
     return months;
@@ -11,7 +17,7 @@ export function getMonthNames({ locale }) {
 
   months = [];
 
-  const formatter = new Intl.DateTimeFormat(locale, { month: 'long' });
+  const formatter = new Intl.DateTimeFormat(locale, { month: style });
   for (let i = 0; i < 12; i += 1) {
     const date = new Date(2019, i, 1);
     const formattedDate = formatter.format(date);
@@ -19,7 +25,8 @@ export function getMonthNames({ locale }) {
     months.push(normalizedDate);
   }
 
-  monthsLocaleCache[locale] = months;
+  monthsLocaleCache[locale] = monthsLocaleCache[locale] || {};
+  monthsLocaleCache[locale][style] = months;
 
   return months;
 }
