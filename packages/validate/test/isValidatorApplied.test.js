ba9b31d7e230e83c3e8ff7e3bfb24be534c1544b
Thijs Louisse <Thijs.Louisse@ing.com>
2019-05-15 16:51:22 +0200

    feat(validate): added isValidatorApplied util

---
+++ packages/validate/test/isValidatorApplied.test.js
@@ -0,0 +1,15 @@
+import { expect } from '@open-wc/testing';
+import { isValidatorApplied } from '../src/isValidatorApplied.js';
+
+describe('isValidatorApplied', () => {
+  it(`checks if validator (provided name string) is applied`, async () => {
+    const myValFn = (val, param) => ({ myValFn: param === 'x' });
+    const myOtherValFn = (val, param) => ({ myOtherValFn: param === 'x' });
+
+    expect(isValidatorApplied('myValFn', myValFn, 'x')).to.equal(true);
+    expect(isValidatorApplied('myValFn', myValFn, 'y')).to.equal(true);
+
+    expect(isValidatorApplied('myValFn', myOtherValFn, 'x')).to.equal(false);
+    expect(isValidatorApplied('myValFn', myOtherValFn, 'y')).to.equal(false);
+  });
+});
