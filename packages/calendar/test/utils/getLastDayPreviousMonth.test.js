9d9063bdfa5af01675ac4c710ca81b7a67203291
Mikhail Bashkirov <mikhail.bashkirov@ing.com>
2019-05-27 14:59:06 +0200

    chore: use native ES modules in karma

--- packages/calendar/test/utils/getLastDayPreviousMonth.test.js
+++ packages/calendar/test/utils/getLastDayPreviousMonth.test.js
@@ -1,11 +1,17 @@
 import { expect } from '@open-wc/testing';
-import { formatDate } from '../../../localize/src/date/formatDate.js';
+import { isSameDate } from '../../src/utils/isSameDate.js';
 import { getLastDayPreviousMonth } from '../../src/utils/getLastDayPreviousMonth.js';
 
 describe('getLastDayPreviousMonth', () => {
   it('returns the last day of the previous month', () => {
-    expect(formatDate(getLastDayPreviousMonth(new Date('2001/01/01')))).to.be.equal('31/12/2000');
-    expect(formatDate(getLastDayPreviousMonth(new Date('2001/10/10')))).to.be.equal('30/09/2001');
-    expect(formatDate(getLastDayPreviousMonth(new Date('2000/03/10')))).to.be.equal('29/02/2000');
+    expect(
+      isSameDate(getLastDayPreviousMonth(new Date('2001/01/01')), new Date('2000/12/31')),
+    ).to.equal(true);
+    expect(
+      isSameDate(getLastDayPreviousMonth(new Date('2001/10/10')), new Date('2001/09/30')),
+    ).to.equal(true);
+    expect(
+      isSameDate(getLastDayPreviousMonth(new Date('2000/03/10')), new Date('2000/02/29')),
+    ).to.equal(true);
   });
 });
