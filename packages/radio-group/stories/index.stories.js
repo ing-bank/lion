1e1981dcabe1e021ea462a7aff37f45144b75be2
Thomas Allmer <d4kmor@gmail.com>
2019-06-12 12:24:00 +0200

    chore: poc for select as radio group

--- packages/radio-group/stories/index.stories.js
+++ packages/radio-group/stories/index.stories.js
@@ -1,10 +1,81 @@
 import { storiesOf, html } from '@open-wc/demoing-storybook';
+// just a POC
+// eslint-disable-next-line
+import { overlays, LocalOverlayController } from '@lion/overlays';
 
 import '../lion-radio-group.js';
 import '@lion/radio/lion-radio.js';
 import '@lion/form/lion-form.js';
+import { LionRadioGroup } from '../src/LionRadioGroup.js';
+
+export class LionSelectRich extends LionRadioGroup {
+  connectedCallback() {
+    super.connectedCallback();
+    this.contentNode = this.querySelector('[slotpoc="content"]');
+    this.invokerNode = this.querySelector('[slotpoc="invoker"]');
+
+    this._popup = overlays.add(
+      new LocalOverlayController({
+        hidesOnEsc: true,
+        hidesOnOutsideClick: false,
+        placement: this.position,
+        contentNode: this.contentNode,
+        invokerNode: this.invokerNode,
+      }),
+    );
+    this._show = () => this._popup.show();
+    this._hide = () => {
+      this._popup.hide();
+    };
+
+    this._toggle = () => {
+      this._popup.toggle();
+    };
+
+    this.invokerNode.addEventListener('click', this._toggle);
+    this.addEventListener('checked-value-changed', ev => {
+      this.invokerNode.innerText = ev.target.checkedValue;
+    });
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.invokerNode.removeEventListener('click', this._toggle);
+  }
+}
+
+customElements.define('lion-select-rich', LionSelectRich);
 
 storiesOf('Forms|Radio Group', module)
+  .add(
+    'SELECT',
+    () => html`
+      <lion-form>
+        <form>
+          <lion-select-rich name="dinosGroup" label="What are your favourite dinosaurs?">
+            <button slotpoc="invoker">click</button>
+            <div slotpoc="content">
+              <lion-radio
+                name="dinos[]"
+                label="allosaurus"
+                .choiceValue=${'allosaurus'}
+              ></lion-radio>
+              <lion-radio
+                name="dinos[]"
+                label="brontosaurus"
+                .choiceValue=${'brontosaurus'}
+              ></lion-radio>
+              <lion-radio
+                name="dinos[]"
+                label="diplodocus"
+                .modelValue=${{ value: 'diplodocus', checked: true }}
+              ></lion-radio>
+            </div>
+          </lion-select-rich>
+        </form>
+      </lion-form>
+    `,
+  )
   .add(
     'Default',
     () => html`
