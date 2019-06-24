c2be4762ab00e1dd044ea057491775a001b38f76
Thijs Louisse <Thijs.Louisse@ing.com>
2019-06-24 16:49:45 +0200

    feat(choice-input): api normalization and cleanup

--- packages/choice-input/test/ChoiceInputMixin.test.js
+++ packages/choice-input/test/ChoiceInputMixin.test.js
@@ -1,5 +1,6 @@
 import { expect, fixture } from '@open-wc/testing';
 import { html } from '@lion/core';
+import sinon from 'sinon';
 
 import { LionInput } from '@lion/input';
 import { ChoiceInputMixin } from '../src/ChoiceInputMixin.js';
@@ -50,11 +51,11 @@ describe('ChoiceInputMixin', () => {
     `);
     expect(counter).to.equal(1); // undefined to set value
 
-    el.choiceChecked = true;
+    el.checked = true;
     expect(counter).to.equal(2);
 
     // no change means no event
-    el.choiceChecked = true;
+    el.checked = true;
     el.choiceValue = 'foo';
     el.modelValue = { value: 'foo', checked: true };
     expect(counter).to.equal(2);
@@ -87,58 +88,136 @@ describe('ChoiceInputMixin', () => {
     `);
 
     expect(el.error.required).to.be.true;
-    el.choiceChecked = true;
+    el.checked = true;
     expect(el.error.required).to.be.undefined;
   });
 
   describe('Checked state synchronization', () => {
     it('synchronizes checked state initially (via attribute or property)', async () => {
       const el = await fixture(`<choice-input></choice-input>`);
-      expect(el.choiceChecked).to.equal(false, 'initially unchecked');
+      expect(el.checked).to.equal(false, 'initially unchecked');
 
       const precheckedElementAttr = await fixture(html`
-        <choice-input .choiceChecked=${true}></choice-input>
+        <choice-input .checked=${true}></choice-input>
       `);
-      expect(precheckedElementAttr.choiceChecked).to.equal(true, 'initially checked via attribute');
+      el._syncValueUpwards = () => {}; // We need to disable the method for the test to pass
+
+      expect(precheckedElementAttr.checked).to.equal(true, 'initially checked via attribute');
     });
 
     it('can be checked and unchecked programmatically', async () => {
       const el = await fixture(`<choice-input></choice-input>`);
-      expect(el.choiceChecked).to.be.false;
-      el.choiceChecked = true;
-      expect(el.choiceChecked).to.be.true;
+      expect(el.checked).to.be.false;
+      el.checked = true;
+      expect(el.checked).to.be.true;
+
+      await el.updateComplete;
+      expect(el.inputElement.checked).to.be.true;
     });
 
     it('can be checked and unchecked via user interaction', async () => {
       const el = await fixture(`<choice-input></choice-input>`);
       el.inputElement.click();
-      expect(el.choiceChecked).to.be.true;
+      expect(el.checked).to.be.true;
       el.inputElement.click();
-      expect(el.choiceChecked).to.be.false;
+      expect(el.checked).to.be.false;
     });
 
     it('synchronizes modelValue to checked state and vice versa', async () => {
       const el = await fixture(html`
         <choice-input .choiceValue=${'foo'}></choice-input>
       `);
-      expect(el.choiceChecked).to.be.false;
+      expect(el.checked).to.be.false;
       expect(el.modelValue).to.deep.equal({
         checked: false,
         value: 'foo',
       });
-      el.choiceChecked = true;
-      expect(el.choiceChecked).to.be.true;
+      el.checked = true;
+      expect(el.checked).to.be.true;
       expect(el.modelValue).to.deep.equal({
         checked: true,
         value: 'foo',
       });
     });
 
-    it('synchronizes checked state to class "state-checked" for styling purposes', async () => {
+    it('ensures optimal synchronize performance by preventing redundant computation steps', async () => {
+      /* we are checking private apis here to make sure we do not have cyclical updates
+        which can be quite common for these type of connected data */
+      const el = await fixture(html`
+        <choice-input .choiceValue=${'foo'}></choice-input>
+      `);
+      expect(el.checked).to.be.false;
+
+      const spyModelCheckedToChecked = sinon.spy(el, '__syncModelCheckedToChecked');
+      const spyCheckedToModel = sinon.spy(el, '__syncCheckedToModel');
+      el.checked = true;
+      expect(el.modelValue.checked).to.be.true;
+      expect(spyModelCheckedToChecked.callCount).to.equal(0);
+      expect(spyCheckedToModel.callCount).to.equal(1);
+
+      el.modelValue = { value: 'foo', checked: false };
+      expect(el.checked).to.be.false;
+      expect(spyModelCheckedToChecked.callCount).to.equal(1);
+      expect(spyCheckedToModel.callCount).to.equal(1);
+
+      // not changeing values should not trigger any updates
+      el.checked = false;
+      el.modelValue = { value: 'foo', checked: false };
+      expect(spyModelCheckedToChecked.callCount).to.equal(1);
+      expect(spyCheckedToModel.callCount).to.equal(1);
+    });
+
+    it('synchronizes checked state to [checked] attribute for styling purposes', async () => {
+      const hasAttr = el => el.hasAttribute('checked');
+      const el = await fixture(`<choice-input></choice-input>`);
+      const elChecked = await fixture(html`
+        <choice-input .checked=${true}>
+          <input slot="input" />
+        </choice-input>
+      `);
+
+      // Initial values
+      expect(hasAttr(el)).to.equal(false, 'inital unchecked element');
+      expect(hasAttr(elChecked)).to.equal(true, 'inital checked element');
+
+      // Programmatically via checked
+      el.checked = true;
+      elChecked.checked = false;
+
+      await el.updateComplete;
+      expect(hasAttr(el)).to.equal(true, 'programmatically checked');
+      expect(hasAttr(elChecked)).to.equal(false, 'programmatically unchecked');
+
+      // reset
+      el.checked = false;
+      elChecked.checked = true;
+
+      // Via user interaction
+      el.inputElement.click();
+      elChecked.inputElement.click();
+      await el.updateComplete;
+      expect(hasAttr(el)).to.equal(true, 'user click checked');
+      expect(hasAttr(elChecked)).to.equal(false, 'user click unchecked');
+
+      // reset
+      el.checked = false;
+      elChecked.checked = true;
+
+      // Programmatically via modelValue
+      el.modelValue = { value: '', checked: true };
+      elChecked.modelValue = { value: '', checked: false };
+      await el.updateComplete;
+      expect(hasAttr(el)).to.equal(true, 'modelValue checked');
+      expect(hasAttr(elChecked)).to.equal(false, 'modelValue unchecked');
+    });
+
+    it('[deprecated] synchronizes checked state to class "state-checked" for styling purposes', async () => {
       const hasClass = el => [].slice.call(el.classList).indexOf('state-checked') > -1;
       const el = await fixture(`<choice-input></choice-input>`);
       const elChecked = await fixture(html`
-        <choice-input .choiceChecked=${true}></choice-input>
+        <choice-input .checked=${true}>
+          <input slot="input" />
+        </choice-input>
       `);
 
       // Initial values
@@ -146,15 +225,16 @@ describe('ChoiceInputMixin', () => {
       expect(hasClass(elChecked)).to.equal(true, 'inital checked element');
 
       // Programmatically via checked
-      el.choiceChecked = true;
-      elChecked.choiceChecked = false;
+      el.checked = true;
+      elChecked.checked = false;
+
       await el.updateComplete;
       expect(hasClass(el)).to.equal(true, 'programmatically checked');
       expect(hasClass(elChecked)).to.equal(false, 'programmatically unchecked');
 
       // reset
-      el.choiceChecked = false;
-      elChecked.choiceChecked = true;
+      el.checked = false;
+      elChecked.checked = true;
 
       // Via user interaction
       el.inputElement.click();
@@ -164,8 +244,8 @@ describe('ChoiceInputMixin', () => {
       expect(hasClass(elChecked)).to.equal(false, 'user click unchecked');
 
       // reset
-      el.choiceChecked = false;
-      elChecked.choiceChecked = true;
+      el.checked = false;
+      elChecked.checked = true;
 
       // Programmatically via modelValue
       el.modelValue = { value: '', checked: true };
@@ -173,6 +253,21 @@ describe('ChoiceInputMixin', () => {
       await el.updateComplete;
       expect(hasClass(el)).to.equal(true, 'modelValue checked');
       expect(hasClass(elChecked)).to.equal(false, 'modelValue unchecked');
+    });
+
+    it('[deprecated] uses choiceChecked to set checked state', async () => {
+      const el = await fixture(html`
+        <choice-input .choiceValue=${'foo'}></choice-input>
+      `);
+      expect(el.choiceChecked).to.be.false;
+      el.choiceChecked = true;
+      expect(el.checked).to.be.true;
+      expect(el.modelValue).to.deep.equal({
+        checked: true,
+        value: 'foo',
+      });
+      await el.updateComplete;
+      expect(el.inputElement.checked).to.be.true;
     });
   });
 
@@ -184,7 +279,7 @@ describe('ChoiceInputMixin', () => {
       expect(el.modelValue).deep.equal({ value: 'foo', checked: false });
 
       const elChecked = await fixture(html`
-        <choice-input .choiceValue=${'foo'} .choiceChecked=${true}></choice-input>
+        <choice-input .choiceValue=${'foo'} .checked=${true}></choice-input>
       `);
       expect(elChecked.modelValue).deep.equal({ value: 'foo', checked: true });
     });
@@ -203,7 +298,7 @@ describe('ChoiceInputMixin', () => {
   describe('Interaction states', () => {
     it('is considered prefilled when checked and not considered prefilled when unchecked', async () => {
       const el = await fixture(html`
-        <choice-input .choiceChecked=${true}></choice-input>
+        <choice-input .checked=${true}></choice-input>
       `);
       expect(el.prefilled).equal(true, 'checked element not considered prefilled');
 
