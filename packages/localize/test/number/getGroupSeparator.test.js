164b32cf691bd446dfd0c541274f07846671ac4f
Mikhail Bashkirov <mikhail.bashkirov@ing.com>
2019-06-27 09:57:51 +0200

    chore(localize): make number formatting tests more scalable

--- packages/localize/test/number/getGroupSeparator.test.js
+++ packages/localize/test/number/getGroupSeparator.test.js
@@ -3,7 +3,7 @@ import { expect } from '@open-wc/testing';
 import { getGroupSeparator } from '../../src/number/getGroupSeparator.js';
 
 describe('getGroupSeparator', () => {
-  it('returns group separator for locale', async () => {
+  it('returns group separator for locale', () => {
     expect(getGroupSeparator('en-GB')).to.equal(',');
     expect(getGroupSeparator('nl-NL')).to.equal('.');
     expect(getGroupSeparator('fr-FR')).to.equal(' ');
