6321777ec7eb7f95f1b60d4d48e51a62d85eaf15
Mikhail Bashkirov <mikhail.bashkirov@ing.com>
2019-07-17 12:27:11 +0200

    chore(localize): remove side effects between tests

--- packages/localize/test-helpers.js
+++ packages/localize/test-helpers.js
@@ -2,6 +2,8 @@ import { localize } from './src/localize.js';
 
 export const localizeTearDown = () => {
   // makes sure that between tests the localization is reset to default state
+  localize._teardownHtmlLangAttributeObserver();
   document.documentElement.lang = 'en-GB';
+  localize._setupHtmlLangAttributeObserver();
   localize.reset();
 };
