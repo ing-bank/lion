e602afe5c6c3ec9d58858414ae6387fbd3d58b3b
gerjanvangeest <gerjan.van.geest@ing.com>
2019-08-05 14:01:27 +0200

    fix(form): sync submit and reset events instead of delegating

--- packages/form/test/lion-form.test.js
+++ packages/form/test/lion-form.test.js
@@ -1,4 +1,4 @@
-import { expect, fixture, html } from '@open-wc/testing';
+import { expect, fixture, html, oneEvent } from '@open-wc/testing';
 import { spy } from 'sinon';
 
 import '@lion/input/lion-input.js';
@@ -7,7 +7,7 @@ import '@lion/fieldset/lion-fieldset.js';
 import '../lion-form.js';
 
 describe('<lion-form>', () => {
-  it.skip('has a custom reset that gets triggered by native reset', async () => {
+  it('has a custom reset that gets triggered by native reset', async () => {
     const withDefaults = await fixture(html`
       <lion-form
         ><form>
@@ -39,6 +39,24 @@ describe('<lion-form>', () => {
     });
   });
 
+  it('dispatches reset events', async () => {
+    const el = await fixture(html`
+      <lion-form>
+        <form>
+          <lion-input name="firstName" .modelValue="${'Foo'}"></lion-input>
+        </form>
+      </lion-form>
+    `);
+
+    setTimeout(() => el.reset());
+    const resetEv = await oneEvent(el, 'reset');
+    expect(resetEv).to.be.instanceOf(Event);
+    expect(resetEv.type).to.equal('reset');
+    expect(resetEv.target).to.equal(el);
+    expect(resetEv.bubbles).to.be.true;
+    expect(resetEv.composed).to.be.false;
+  });
+
   it('works with the native submit event (triggered via a button)', async () => {
     const submitSpy = spy();
     const el = await fixture(html`
@@ -52,5 +70,23 @@ describe('<lion-form>', () => {
     const button = el.querySelector('button');
     button.click();
     expect(submitSpy.callCount).to.equal(1);
+  });
+
+  it('dispatches submit events', async () => {
+    const el = await fixture(html`
+      <lion-form>
+        <form>
+          <button type="submit">submit</button>
+        </form>
+      </lion-form>
+    `);
+    const button = el.querySelector('button');
+    setTimeout(() => button.click());
+    const submitEv = await oneEvent(el, 'submit');
+    expect(submitEv).to.be.instanceOf(Event);
+    expect(submitEv.type).to.equal('submit');
+    expect(submitEv.target).to.equal(el);
+    expect(submitEv.bubbles).to.be.true;
+    expect(submitEv.composed).to.be.false;
   });
 });
