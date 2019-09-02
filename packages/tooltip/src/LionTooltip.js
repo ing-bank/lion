73e2305164a58754b99b329ef52e3878a29de8b6
erik <erik.kroes@ing.com>
2019-09-02 15:17:31 +0200

    fix(tooltip): make tooltip hoverable

--- packages/tooltip/src/LionTooltip.js
+++ packages/tooltip/src/LionTooltip.js
@@ -20,16 +20,16 @@ export class LionTooltip extends LionPopup {
     this._show = () => this._controller.show();
     this._hide = () => this._controller.hide();
 
-    this.invokerNode.addEventListener('mouseenter', this._show);
-    this.invokerNode.addEventListener('mouseleave', this._hide);
+    this.addEventListener('mouseenter', this._show);
+    this.addEventListener('mouseleave', this._hide);
     this.invokerNode.addEventListener('focusin', this._show);
     this.invokerNode.addEventListener('focusout', this._hide);
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
-    this.invokerNode.removeEventListener('mouseenter', this._show);
-    this.invokerNode.removeEventListener('mouseleave', this._hide);
+    this.removeEventListener('mouseenter', this._show);
+    this.removeEventListener('mouseleave', this._hide);
     this.invokerNode.removeEventListener('focusin', this._show);
     this.invokerNode.removeEventListener('focusout', this._hide);
   }
