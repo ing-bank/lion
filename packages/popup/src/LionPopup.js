49c28854e75067f595763c262435391336269f94
Thijs Louisse <Thijs.Louisse@ing.com>
2019-10-08 08:47:05 +0200

    feat(popup): use OverlayMixin

--- packages/popup/src/LionPopup.js
+++ packages/popup/src/LionPopup.js
@@ -1,53 +1,33 @@
-import { UpdatingElement } from '@lion/core';
-import { overlays, LocalOverlayController } from '@lion/overlays';
+import { LitElement } from '@lion/core';
+import { OverlayMixin, OverlayController } from '@lion/overlays';
 
-export class LionPopup extends UpdatingElement {
-  static get properties() {
-    return {
-      popperConfig: {
-        type: Object,
-      },
-    };
+export class LionPopup extends OverlayMixin(LitElement) {
+  get _overlayContentNode() {
+    return this.querySelector('[slot=content]');
   }
 
-  get popperConfig() {
-    return this._popperConfig;
+  get _overlayInvokerNode() {
+    return this.querySelector('[slot=invoker]');
   }
 
-  set popperConfig(config) {
-    this._popperConfig = {
-      ...this._popperConfig,
-      ...config,
-    };
-
-    if (this._controller && this._controller._popper) {
-      this._controller.updatePopperConfig(this._popperConfig);
-    }
+  // eslint-disable-next-line class-methods-use-this
+  _defineOverlay() {
+    return new OverlayController({
+      placementMode: 'local',
+      contentNode: this._overlayContentNode,
+      invokerNode: this._overlayInvokerNode,
+      handlesAccessibility: true,
+    });
   }
 
   connectedCallback() {
     super.connectedCallback();
-    this.contentNode = this.querySelector('[slot="content"]');
-    this.invokerNode = this.querySelector('[slot="invoker"]');
-
-    this._controller = overlays.add(
-      new LocalOverlayController({
-        hidesOnEsc: true,
-        hidesOnOutsideClick: true,
-        popperConfig: this.popperConfig,
-        contentNode: this.contentNode,
-        invokerNode: this.invokerNode,
-      }),
-    );
-    this._show = () => this._controller.show();
-    this._hide = () => this._controller.hide();
-    this._toggle = () => this._controller.toggle();
-
-    this.invokerNode.addEventListener('click', this._toggle);
+    this.__toggle = () => this._overlayCtrl.toggle();
+    this._overlayInvokerNode.addEventListener('click', this.__toggle);
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
-    this.invokerNode.removeEventListener('click', this._toggle);
+    this._overlayInvokerNode.removeEventListener('click', this._toggle);
   }
 }
