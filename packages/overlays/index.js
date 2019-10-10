8796399c7176b0d7644b1144fba371397b784bf8
Thomas Allmer <thomas.allmer@ing.com>
2019-10-10 13:49:24 +0200

    chore: have multiple configuration files

--- packages/overlays/index.js
+++ packages/overlays/index.js
@@ -4,8 +4,6 @@ export { OverlaysManager } from './src/OverlaysManager.js';
 export { OverlayController } from './src/OverlayController.js';
 export { OverlayMixin } from './src/OverlayMixin.js';
 
-export {
-  withBottomSheetConfig,
-  withModalDialogConfig,
-  withDropdownConfig,
-} from './src/utils/controller-configurations.js';
+export { withBottomSheetConfig } from './src/configurations/withBottomSheetConfig.js';
+export { withModalDialogConfig } from './src/configurations/withModalDialogConfig.js';
+export { withDropdownConfig } from './src/configurations/withDropdownConfig.js';
