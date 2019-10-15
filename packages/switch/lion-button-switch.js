a90a5ed5300f6205a863536ce8f48ae263bdc6e4
Alex Ghiu <alex.ghiu@ing.com>
2019-10-15 11:10:17 +0200

    feat(switch): component to toggle a property on or off

---
+++ packages/switch/lion-button-switch.js
@@ -0,0 +1,3 @@
+import { LionButtonSwitch } from './src/LionButtonSwitch.js';
+
+customElements.define('lion-button-switch', LionButtonSwitch);
---
+++ packages/switch/test/lion-button-switch.test.js
@@ -0,0 +1,114 @@
+import { expect, fixture, html } from '@open-wc/testing';
+import sinon from 'sinon';
+
+import { LionButton } from '@lion/button';
+
+import '../lion-button-switch.js';
+
+describe('lion-button-switch', () => {
+  let el;
+  beforeEach(async () => {
+    el = await fixture(html`
+      <lion-button-switch></lion-button-switch>
+    `);
+  });
+
+  it('should behave like a button', () => {
+    expect(el instanceof LionButton).to.be.true;
+  });
+
+  it('should be focusable', () => {
+    expect(el.tabIndex).to.equal(0);
+    expect(el.getAttribute('tabindex')).to.equal('0');
+  });
+
+  it('should not have a [type]', () => {
+    expect(el.hasAttribute('type')).to.be.false;
+  });
+
+  it('should have checked=false by default', () => {
+    expect(el.checked).to.equal(false);
+    expect(el.hasAttribute('checked')).to.be.false;
+  });
+
+  it('should toggle the value of "checked" on click', async () => {
+    expect(el.checked).to.be.false;
+    expect(el.hasAttribute('checked')).to.be.false;
+    el.click();
+    await el.updateComplete;
+    expect(el.checked).to.be.true;
+    expect(el.hasAttribute('checked')).to.be.true;
+    el.click();
+    await el.updateComplete;
+    expect(el.checked).to.be.false;
+    expect(el.hasAttribute('checked')).to.be.false;
+  });
+
+  it('can be disabled', async () => {
+    el.disabled = true;
+    expect(el.checked).to.be.false;
+    el.click();
+    await el.updateComplete;
+    expect(el.checked).to.be.false;
+    expect(el.hasAttribute('checked')).to.be.false;
+    el.disabled = true;
+    el.checked = true;
+    el.click();
+    await el.updateComplete;
+    expect(el.checked).to.be.true;
+    expect(el.hasAttribute('checked')).to.be.true;
+  });
+
+  it('should dispatch "checked-changed" event when toggled', () => {
+    const handlerSpy = sinon.spy();
+    el.addEventListener('checked-changed', handlerSpy);
+    el.click();
+    el.click();
+    expect(handlerSpy.callCount).to.equal(2);
+    const checkCall = call => {
+      expect(call.args).to.have.a.lengthOf(1);
+      const e = call.args[0];
+      expect(e).to.be.an.instanceof(Event);
+      expect(e.bubbles).to.be.true;
+      expect(e.composed).to.be.true;
+    };
+    checkCall(handlerSpy.getCall(0), true);
+    checkCall(handlerSpy.getCall(1), false);
+  });
+
+  it('should not dispatch "checked-changed" event if disabled', () => {
+    const handlerSpy = sinon.spy();
+    el.disabled = true;
+    el.addEventListener('checked-changed', handlerSpy);
+    el.click();
+    expect(handlerSpy.called).to.be.false;
+  });
+
+  describe('a11y', () => {
+    it('should manage "aria-checked"', async () => {
+      expect(el.hasAttribute('aria-checked')).to.be.true;
+      expect(el.getAttribute('aria-checked')).to.equal('false');
+
+      el.click();
+      await el.updateComplete;
+      expect(el.getAttribute('aria-checked')).to.equal('true');
+      el.click();
+      await el.updateComplete;
+      expect(el.getAttribute('aria-checked')).to.equal('false');
+
+      el.checked = true;
+      await el.updateComplete;
+      expect(el.getAttribute('aria-checked')).to.equal('true');
+      el.checked = false;
+      await el.updateComplete;
+      expect(el.getAttribute('aria-checked')).to.equal('false');
+
+      el.setAttribute('checked', true);
+      await el.updateComplete;
+      expect(el.getAttribute('aria-checked')).to.equal('true');
+      el.removeAttribute('checked');
+      await el.updateComplete;
+      expect(el.getAttribute('aria-checked')).to.equal('false');
+    });
+  });
+});
