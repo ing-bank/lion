67e28ece2eaae3307409e437772981f9d9b819f6
Pavlik (ud40ht) Kiselev <pavlik.kiselev@ing.com>
2019-10-23 17:57:13 +0200

    fix(localize): Fixed the parse of dates like "D-M-Y" and allow only "D-M-YYYY" with a full year

--- packages/localize/src/date/parseDate.js
+++ packages/localize/src/date/parseDate.js
@@ -26,32 +26,40 @@ const memoizedGetDateFormatBasedOnLocale = memoize(getDateFormatBasedOnLocale, l
 export function parseDate(date) {
   const stringToParse = addLeadingZero(date);
   let parsedString;
+  let isFullYear = false;
   switch (memoizedGetDateFormatBasedOnLocale()) {
     case 'day-month-year':
-      parsedString = `${stringToParse.slice(6, 10)}/${stringToParse.slice(
-        3,
-        5,
-      )}/${stringToParse.slice(0, 2)}`;
+      isFullYear = stringToParse.length === 10;
+      parsedString = [
+        stringToParse.slice(6, 10),
+        stringToParse.slice(3, 5),
+        stringToParse.slice(0, 2),
+      ].join('/');
       break;
     case 'month-day-year':
-      parsedString = `${stringToParse.slice(6, 10)}/${stringToParse.slice(
-        0,
-        2,
-      )}/${stringToParse.slice(3, 5)}`;
+      isFullYear = stringToParse.length === 10;
+      parsedString = [
+        stringToParse.slice(6, 10),
+        stringToParse.slice(0, 2),
+        stringToParse.slice(3, 5),
+      ].join('/');
       break;
     case 'year-month-day':
-      parsedString = `${stringToParse.slice(0, 4)}/${stringToParse.slice(
-        5,
-        7,
-      )}/${stringToParse.slice(8, 10)}`;
+      isFullYear = /\d+/.test(stringToParse.slice(0, 4));
+      parsedString = [
+        stringToParse.slice(0, 4),
+        stringToParse.slice(5, 7),
+        stringToParse.slice(8, 10),
+      ].join('/');
       break;
     default:
       parsedString = '0000/00/00';
   }
+
   const parsedDate = new Date(parsedString);
   // Check if parsedDate is not `Invalid Date`
   // eslint-disable-next-line no-restricted-globals
-  if (!isNaN(parsedDate)) {
+  if (!isNaN(parsedDate) && isFullYear) {
     return parsedDate;
   }
   return undefined;
