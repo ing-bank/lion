c9719ad34228296fd83e8195ead1c543f2ccb609
Thijs Louisse <Thijs.Louisse@ing.com>
2019-10-30 10:55:16 +0100

    feat(input): updated stories validate system changes

--- packages/input/stories/validation-string.stories.js
+++ packages/input/stories/validation-string.stories.js
@@ -1,131 +1,189 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf, html } from '@open-wc/demoing-storybook';
 import {
-  equalsLengthValidator,
-  minLengthValidator,
-  maxLengthValidator,
-  minMaxLengthValidator,
-  isEmailValidator,
+  Required,
+  EqualsLength,
+  MinDate,
+  MinLength,
+  MaxLength,
+  MinMaxLength,
+  Validator,
+  loadDefaultFeedbackMessages,
 } from '@lion/validate';
-import { LocalizeMixin } from '@lion/localize';
-import { LionInput } from '../index.js';
+import '@lion/input-date/lion-input-date.js';
+import '@lion/input/lion-input.js';
+import { DefaultSuccess } from '@lion/validate/src/resultValidators/DefaultSuccess.js';
+
+loadDefaultFeedbackMessages();
 
 storiesOf('Forms|Input String Validation', module)
   .add(
-    'equalsLength',
+    'String Validators',
     () => html`
       <lion-input
-        .errorValidators=${[equalsLengthValidator(7)]}
-        .modelValue=${'not exactly'}
-        label="equalsLengthValidator"
+        .validators="${[new EqualsLength(7)]}"
+        .modelValue="${'not exactly'}"
+        label="EqualsLength"
       ></lion-input>
       <lion-input
-        .errorValidators=${[equalsLengthValidator(7)]}
-        .modelValue=${'exactly'}
-        label="equalsLengthValidator"
+        .validators="${[new MaxLength(7)]}"
+        .modelValue="${'exactly'}"
+        label="MaxLength"
       ></lion-input>
-    `,
-  )
-  .add(
-    'minLength',
-    () => html`
+
       <lion-input
-        .errorValidators=${[minLengthValidator(10)]}
+        .validators=${[new MinLength(10)]}
         .modelValue=${'too short'}
-        label="minLengthValidator"
+        label="MinLength"
       ></lion-input>
       <lion-input
-        .errorValidators=${[minLengthValidator(10)]}
+        .validators=${[new MinMaxLength(10, 20)]}
         .modelValue=${'that should be enough'}
-        label="minLengthValidator"
+        label="MinMaxLength"
       ></lion-input>
     `,
   )
+  // TODO: add Number validators
+  // TODO: fix input-date and add Date Validators
   .add(
-    'maxLength',
+    'Types',
     () => html`
+      <style>
+        lion-input[has-success] input {
+          outline: 2px solid green;
+        }
+        lion-input[has-error] input {
+          outline: 2px solid red;
+        }
+        lion-input[has-warning] input {
+          outline: 2px solid orange;
+        }
+      </style>
       <lion-input
-        .errorValidators=${[maxLengthValidator(13)]}
-        .modelValue=${'too long it seems'}
-        label="maxLengthValidator"
-      ></lion-input>
-      <lion-input
-        .errorValidators=${[maxLengthValidator(13)]}
-        .modelValue=${'just perfect'}
-        label="maxLengthValidator"
+        .validators="${[
+          new Required(),
+          new MinLength(7, { type: 'warning' }),
+          new MaxLength(10, { type: 'info' }),
+          new DefaultSuccess(),
+        ]}"
+        .modelValue="${'exactly'}"
+        label="MaxLength"
       ></lion-input>
     `,
   )
-  .add(
-    'minMaxLength',
-    () => html`
-      <lion-input
-        .errorValidators=${[minMaxLengthValidator({ min: 10, max: 13 })]}
-        .modelValue=${'too short'}
-        label="minMaxLengthValidator"
-      ></lion-input>
-      <lion-input
-        .errorValidators=${[minMaxLengthValidator({ min: 10, max: 13 })]}
-        .modelValue=${'too long it seems'}
-        label="minMaxLengthValidator"
-      ></lion-input>
-      <lion-input
-        .errorValidators=${[minMaxLengthValidator({ min: 10, max: 13 })]}
-        .modelValue=${'just perfect'}
-        label="minMaxLengthValidator"
-      ></lion-input>
-    `,
-  )
-  .add(
-    'isEmail',
-    () => html`
-      <lion-input
-        .errorValidators=${[isEmailValidator()]}
-        .modelValue=${'foo'}
-        label="isEmailValidator"
-      ></lion-input>
-      <lion-input
-        .errorValidators=${[isEmailValidator()]}
-        .modelValue=${'foo@bar.com'}
-        label="isEmailValidator"
-      ></lion-input>
-    `,
-  )
-  .add('error/warning/info/success states', () => {
-    class InputValidationExample extends LocalizeMixin(LionInput) {
-      static get localizeNamespaces() {
-        return [
-          { 'input-validation-example': locale => import(`./translations/${locale}.js`) },
-          ...super.localizeNamespaces,
-        ];
+  .add('Custom Validator', () => {
+    class MyValidator extends Validator {
+      constructor(...args) {
+        super(...args);
+        this.name = 'myValidator';
+      }
+
+      execute(modelValue, param) {
+        return modelValue !== param;
+      }
+
+      static getMessage({ fieldName, modelValue, validatorParams: param }) {
+        if (modelValue.length >= param.length - 1 && param.startsWith(modelValue)) {
+          return 'Almost there...';
+        }
+        return `No "${param}" found in ${fieldName}`;
       }
     }
-    if (!customElements.get('input-validation-example')) {
-      customElements.define('input-validation-example', InputValidationExample);
+
+    return html`
+      <lion-input
+        label="Custom validator"
+        help-text="Type 'mine' please"
+        .validators="${[new MyValidator('mine')]}"
+        .modelValue="${'mi'}"
+      ></lion-input>
+    `;
+  })
+  .add(
+    'Override default messages',
+    () => html`
+      <lion-input
+        .validators="${[new EqualsLength(4, { getMessage: () => '4 chars please...' })]}"
+        .modelValue="${'123'}"
+        label="Custom message for validator instance"
+      ></lion-input>
+      <lion-input
+        .validators="${[
+          new EqualsLength(4, {
+            getMessage: ({ modelValue, validatorParams: param }) => {
+              const diff = modelValue.length - param;
+              return `${Math.abs(diff)} too ${diff > 0 ? 'much' : 'few'}...`;
+            },
+          }),
+        ]}"
+        .modelValue="${'way too much'}"
+        label="Dynamic message for validator instance"
+      ></lion-input>
+    `,
+  )
+  .add('Asynchronous validation', () => {
+    function pause(ms = 0) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
     }
 
-    const notEqualsString = (value, stringValue) => stringValue.toString() !== value;
-    const notEqualsStringValidator = (...factoryParams) => [
-      (...params) => ({ notEqualsString: notEqualsString(...params) }),
-      factoryParams,
-    ];
-    const equalsStringFixedValidator = () => [() => ({ notEqualsStringFixed: false })];
+    class AsyncValidator extends Validator {
+      constructor(...args) {
+        super(...args);
+        this.name = 'asyncValidator';
+        this.async = true;
+      }
+
+      async execute() {
+        console.log('async pending...');
+        await pause(2000);
+        console.log('async done...');
+        return true;
+      }
+
+      static getMessage({ modelValue }) {
+        return `validated for modelValue: ${modelValue}...`;
+      }
+    }
+
     return html`
-      <input-validation-example
-        .errorValidators=${[notEqualsStringValidator('error')]}
-        .successValidators=${[equalsStringFixedValidator()]}
-        .modelValue=${'error'}
-        label="Error"
-        help-text="Clearing the error (add a character) will show a success message"
-      ></input-validation-example>
-      <input-validation-example
-        .warningValidators=${[notEqualsStringValidator('warning')]}
-        .modelValue=${'warning'}
-        label="Warning"
-      ></input-validation-example>
-      <input-validation-example
-        .infoValidators=${[notEqualsStringValidator('info')]}
-        .modelValue=${'info'}
-        label="Info"
-      ></input-validation-example>
+      <style>
+        lion-input[is-pending] {
+          opacity: 0.5;
+        }
+      </style>
+      <lion-input
+        label="Async validation"
+        .validators="${[new AsyncValidator()]}"
+        .modelValue="${'123'}"
+      ></lion-input>
+    `;
+  })
+  .add('Dynamic parameter changes', () => {
+    const beginDate = new Date('09/09/1990');
+    const minDateValidatorRef = new MinDate(beginDate, {
+      message: 'Fill in a date after your birth date',
+    });
+
+    return html`
+      <lion-input-date
+        label="Your birth date"
+        help-text="Adjust this date to retrigger validation of the input below..."
+        .modelValue="${beginDate}"
+        @model-value-changed="${({ target: { modelValue, errorState } }) => {
+          if (!errorState) {
+            // Since graduation date is usually not before birth date
+            minDateValidatorRef.param = modelValue;
+          }
+        }}"
+      ></lion-input-date>
+      <lion-input-date
+        label="Your graduation date"
+        .modelValue="${new Date('09/09/1989')}"
+        .validators="${[minDateValidatorRef]}"
+      ></lion-input-date>
     `;
   });
