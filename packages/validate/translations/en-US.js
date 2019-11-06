96df336226919fe3202b9750d0381a9c44b30ecd
Menno Luiten <menno.luiten@ing.com>
2019-11-06 12:00:49 +0100

    fix(validate): en-US translations

--- packages/validate/translations/en-US.js
+++ packages/validate/translations/en-US.js
@@ -3,9 +3,11 @@ import en from './en.js';
 export default {
   ...en,
   error: {
+    ...en.error,
     isDate: 'Please enter a valid date (MM/DD/YYYY).',
   },
   warning: {
+    ...en.warning,
     isDate: 'Please enter a valid date (MM/DD/YYYY).',
   },
 };
