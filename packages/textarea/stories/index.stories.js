0a271e052962fc429ff825cdafa967fcd8fd7605
qa46hx <Gerjan.van.Geest@ing.com>
2019-11-06 16:17:51 +0100

    chore(textarea): update to new validators

--- packages/textarea/stories/index.stories.js
+++ packages/textarea/stories/index.stories.js
@@ -1,6 +1,8 @@
 import { storiesOf, html } from '@open-wc/demoing-storybook';
-
+import { Required, MinLength, MaxLength } from '@lion/validate';
 import '../lion-textarea.js';
+
+const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
 storiesOf('Forms|Textarea', module)
   .add(
@@ -40,5 +42,15 @@ storiesOf('Forms|Textarea', module)
     () => html`
       <p>To have a fixed size provide rows and maxRows with the same value</p>
       <lion-textarea label="Non Growing" rows="3" max-rows="3"></lion-textarea>
+    `,
+  )
+  .add(
+    'Validators',
+    () => html`
+      <lion-textarea
+        .validators="${[new Required(), new MinLength(10), new MaxLength(400)]}"
+        label="Validation"
+        .modelValue="${lorem}"
+      ></lion-textarea>
     `,
   );
--- packages/textarea/test/lion-textarea-integrations.test.js
+++ packages/textarea/test/lion-textarea-integrations.test.js
@@ -1,9 +1,10 @@
 import { runFormatMixinSuite } from '@lion/field/test-suites/FormatMixin.suite.js';
+
 import '../lion-textarea.js';
 
 const tagString = 'lion-textarea';
 
-describe('<lion-textarea> integrations', () => {
+describe.only('<lion-textarea> integrations', () => {
   runFormatMixinSuite({
     tagString,
   });
