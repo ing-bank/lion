9e33ac587f9c5b7c489456aaeba46ab69564f190
qa46hx <Gerjan.van.Geest@ing.com>
2019-11-13 15:51:06 +0100

    chore(form): update to new validators

--- packages/form/stories/index.stories.js
+++ packages/form/stories/index.stories.js
@@ -5,7 +5,7 @@ import '@lion/fieldset/lion-fieldset.js';
 import '@lion/input-iban/lion-input-iban.js';
 import '@lion/textarea/lion-textarea.js';
 
-import { maxLengthValidator } from '@lion/validate';
+import { Required, MaxLength } from '@lion/validate';
 
 storiesOf('Forms|Form', module)
   .add(
@@ -50,13 +50,13 @@ storiesOf('Forms|Form', module)
             <lion-input
               name="firstName"
               label="First Name"
-              .errorValidators=${['required', maxLengthValidator(15)]}
+              .validators=${[new Required(), new MaxLength(15)]}
             >
             </lion-input>
             <lion-input
               name="lastName"
               label="Last Name"
-              .errorValidators=${['required', maxLengthValidator(15)]}
+              .validators=${[new Required(), new MaxLength(15)]}
             >
             </lion-input>
           </lion-fieldset>
