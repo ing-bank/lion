cc292d86df3074a921af35ef6f554e919c192819
Lars den Bakker <larsdenbakker@gmail.com>
2019-11-27 17:33:45 +0100

    feat(checkbox-group): add checked and serialized value

--- packages/checkbox-group/src/LionCheckboxGroup.js
+++ packages/checkbox-group/src/LionCheckboxGroup.js
@@ -1,6 +1,33 @@
 import { LionFieldset } from '@lion/fieldset';
 
 export class LionCheckboxGroup extends LionFieldset {
+  get checkedValue() {
+    return this.serializedValue;
+  }
+
+  set checkedValue(values) {
+    this.serializedValue = values;
+  }
+
+  get serializedValue() {
+    return this._getCheckedCheckboxElements().map(x => x.modelValue.value);
+  }
+
+  set serializedValue(values) {
+    this._setCheckedCheckboxElements(values);
+  }
+
+  _getCheckedCheckboxElements() {
+    return this.formElementsArray.filter(el => el.checked);
+  }
+
+  _setCheckedCheckboxElements(values) {
+    this.formElementsArray.forEach(element => {
+      /* eslint-disable-next-line no-param-reassign */
+      element.checked = values.includes(element.modelValue.value);
+    });
+  }
+
   // eslint-disable-next-line class-methods-use-this
   _isEmpty(modelValues) {
     const keys = Object.keys(modelValues);
--- packages/checkbox-group/test/lion-checkbox-group.test.js
+++ packages/checkbox-group/test/lion-checkbox-group.test.js
@@ -25,4 +25,88 @@ describe('<lion-checkbox-group>', () => {
     el.formElements['sports[]'][0].checked = true;
     expect(el.hasFeedbackFor).to.deep.equal([]);
   });
+
+  describe('serializedValue', async () => {
+    it('reflects the current checked state', async () => {
+      const el = await fixture(html`
+        <lion-checkbox-group>
+          <lion-checkbox name="sports[]" .choiceValue=${'running'}></lion-checkbox>
+          <lion-checkbox name="sports[]" .choiceValue=${'swimming'}></lion-checkbox>
+        </lion-checkbox-group>
+      `);
+      await el.registrationReady;
+
+      el.modelValue = {
+        'sports[]': [{ value: 'running', checked: true }, { value: 'swimming', checked: true }],
+      };
+      expect(el.serializedValue).to.eql(['running', 'swimming']);
+
+      el.modelValue = {
+        'sports[]': [{ value: 'running', checked: false }, { value: 'swimming', checked: false }],
+      };
+      expect(el.serializedValue).to.eql([]);
+    });
+
+    it('can bet set programmatically', async () => {
+      const el = await fixture(html`
+        <lion-checkbox-group>
+          <lion-checkbox name="sports[]" .choiceValue=${'running'}></lion-checkbox>
+          <lion-checkbox name="sports[]" .choiceValue=${'swimming'}></lion-checkbox>
+        </lion-checkbox-group>
+      `);
+      await el.registrationReady;
+
+      el.serializedValue = ['running'];
+      expect(el.modelValue).to.eql({
+        'sports[]': [{ value: 'running', checked: true }, { value: 'swimming', checked: false }],
+      });
+
+      el.serializedValue = ['running', 'swimming'];
+      expect(el.modelValue).to.eql({
+        'sports[]': [{ value: 'running', checked: true }, { value: 'swimming', checked: true }],
+      });
+    });
+  });
+
+  describe('checkedValue', async () => {
+    it('reflects the current checked state', async () => {
+      const el = await fixture(html`
+        <lion-checkbox-group>
+          <lion-checkbox name="sports[]" .choiceValue=${'running'}></lion-checkbox>
+          <lion-checkbox name="sports[]" .choiceValue=${'swimming'}></lion-checkbox>
+        </lion-checkbox-group>
+      `);
+      await el.registrationReady;
+
+      el.modelValue = {
+        'sports[]': [{ value: 'running', checked: true }, { value: 'swimming', checked: true }],
+      };
+      expect(el.checkedValue).to.eql(['running', 'swimming']);
+
+      el.modelValue = {
+        'sports[]': [{ value: 'running', checked: false }, { value: 'swimming', checked: false }],
+      };
+      expect(el.checkedValue).to.eql([]);
+    });
+
+    it('can bet set programmatically', async () => {
+      const el = await fixture(html`
+        <lion-checkbox-group>
+          <lion-checkbox name="sports[]" .choiceValue=${'running'}></lion-checkbox>
+          <lion-checkbox name="sports[]" .choiceValue=${'swimming'}></lion-checkbox>
+        </lion-checkbox-group>
+      `);
+      await el.registrationReady;
+
+      el.checkedValue = ['running'];
+      expect(el.modelValue).to.eql({
+        'sports[]': [{ value: 'running', checked: true }, { value: 'swimming', checked: false }],
+      });
+
+      el.checkedValue = ['running', 'swimming'];
+      expect(el.modelValue).to.eql({
+        'sports[]': [{ value: 'running', checked: true }, { value: 'swimming', checked: true }],
+      });
+    });
+  });
 });
