ae3080dd83f55f5af1dbcc4a43266e60c5c16bcf
CubLion <alex.ghiu@ing.com>
2020-01-09 15:21:18 +0100

    chore: move sb-locale-switcher to helpers

--- packages/helpers/index.js
+++ packages/helpers/index.js
@@ -3,3 +3,4 @@ export { renderLitAsNode } from './renderLitAsNode/src/renderLitAsNode.js';
 
 // Components
 export { SbActionLogger } from './sb-action-logger/src/SbActionLogger.js';
+export { SbLocaleSwitcher } from './sb-locale-switcher/src/SbLocaleSwitcher.js';
