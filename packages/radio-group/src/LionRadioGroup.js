396d03b3dde1f6c8162d064127a72264523d6510
Thomas Allmer <thomas.allmer@ing.com>
2020-02-10 19:15:39 +0100

    fix: move documentation from readme to mdx

--- packages/radio-group/src/LionRadioGroup.js
+++ packages/radio-group/src/LionRadioGroup.js
@@ -2,31 +2,13 @@ import { ChoiceGroupMixin } from '@lion/choice-input';
 import { LionFieldset } from '@lion/fieldset';
 
 /**
- * LionRadioGroup: extends the lion-fieldset
+ * A wrapper around multiple radios.
  *
- * <lion-radio-group name="radios">
- *   <label slot="label">My Radio</label>
- *   <lion-radio>
- *     <label slot="label">Male</label>
- *   </lion-radio>
- *   <lion-radio>
- *     <label slot="label">Female</label>
- *   </lion-radio>
- * </lion-radio-group>
- *
- * You can preselect an option by setting marking an lion-radio checked.
- *   Example:
- *   <lion-radio checked></lion-radio>
- *
- * It extends LionFieldset so it inherits it's features.
- *
- *
- * @customElement lion-radio-group
  * @extends {LionFieldset}
  */
-
 export class LionRadioGroup extends ChoiceGroupMixin(LionFieldset) {
   connectedCallback() {
+    // eslint-disable-next-line wc/guard-super-call
     super.connectedCallback();
     this._setRole('radiogroup');
   }
