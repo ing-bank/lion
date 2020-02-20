f44d8aa26ae7124d8dcb251e1f66ab9beae71050
Thijs Louisse <Thijs.Louisse@ing.com>
2020-02-20 11:35:41 +0100

    fix: removed FieldCustomMixin

--- packages/field/index.js
+++ packages/field/index.js
@@ -1,6 +1,5 @@
 export { FocusMixin } from './src/FocusMixin.js';
 export { FormatMixin } from './src/FormatMixin.js';
-export { FieldCustomMixin } from './src/FieldCustomMixin.js';
 export { FormControlMixin } from './src/FormControlMixin.js';
 export { InteractionStateMixin } from './src/InteractionStateMixin.js'; // applies FocusMixin
 export { LionField } from './src/LionField.js';
