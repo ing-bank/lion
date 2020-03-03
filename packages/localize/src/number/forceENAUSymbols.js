c025aa9856cc4b3b4338b1e488e7dd12c50f5059
Sandipan Basak <sandipan329@gmail.com>
2020-03-03 23:59:09 +0530

    fix: replaced nested if's by switch

--- packages/localize/src/number/forceENAUSymbols.js
+++ packages/localize/src/number/forceENAUSymbols.js
@@ -1,18 +1,21 @@
 export function forceENAUSymbols(formattedParts, options) {
   const result = formattedParts;
   const numberOfParts = result.length;
-  // Change the symbol from JPY to ¥, due to bug in Chrome
+  // Change the symbols for locale 'en-AU', due to bug in Chrome
   if (numberOfParts > 1 && options && options.currencyDisplay === 'symbol') {
-    if (options.currency === 'EUR') {
-      result[0].value = '€';
-      result[1].value = '';
-    } else if (options.currency === 'USD') {
-      result[0].value = '$';
-      result[1].value = '';
-    } else if (options.currency === 'JPY') {
-      result[0].value = '¥';
-      result[1].value = '';
+    switch (options.currency) {
+      case 'EUR':
+        result[0].value = '€';
+        break;
+      case 'USD':
+        result[0].value = '$';
+        break;
+      case 'JPY':
+        result[0].value = '¥';
+        break;
+      /* no default */
     }
+    result[1].value = '';
   }
   return result;
 }
