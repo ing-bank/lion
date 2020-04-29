ec057e3fefa4ef96f865f3cb054886a1383f8b26
qa46hx <Gerjan.van.Geest@ing.com>
2020-04-29 16:05:45 +0200

    chore: upgrade open-wc/demoing-storybook rollup versions

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,42 +7,49 @@ module.exports = {
     '../packages/helpers/*/stories/*.stories.{js,mdx}',
     '../demo/stories/*.stories.{js,mdx}',
   ],
+  addons: [
+    'storybook-prebuilt/addon-docs/register.js',
+    'storybook-prebuilt/addon-actions/register.js',
+    'storybook-prebuilt/addon-knobs/register.js',
+    'storybook-prebuilt/addon-a11y/register.js',
+    'storybook-prebuilt/addon-backgrounds/register.js',
+    'storybook-prebuilt/addon-links/register.js',
+    'storybook-prebuilt/addon-viewport/register.js',
+  ],
   esDevServer: {
     nodeResolve: true,
     watch: true,
     open: true,
   },
-  rollup: configs => {
+  rollup: config => {
     // temporarily hard copy all needed global files as all tested rollup plugins flatten the
     // directory structure
     // `rollup-plugin-copy` might work if issue 37 is resolved
     // https://github.com/vladshcherbin/rollup-plugin-copy/issues/37
-    for (const config of configs) {
-      config.plugins.push({
-        generateBundle() {
-          this.emitFile({
-            type: 'asset',
-            fileName: 'packages/form-system/dev-assets/FormatMixinDiagram-1.svg',
-            source: fs.readFileSync(
-              path.join(__dirname, '../packages/form-system/dev-assets/FormatMixinDiagram-1.svg'),
-            ),
-          });
-          this.emitFile({
-            type: 'asset',
-            fileName: 'packages/form-system/dev-assets/FormatMixinDiagram-2.svg',
-            source: fs.readFileSync(
-              path.join(__dirname, '../packages/form-system/dev-assets/FormatMixinDiagram-2.svg'),
-            ),
-          });
-          this.emitFile({
-            type: 'asset',
-            fileName: 'packages/form-system/dev-assets/FormatMixinDiagram-3.svg',
-            source: fs.readFileSync(
-              path.join(__dirname, '../packages/form-system/dev-assets/FormatMixinDiagram-3.svg'),
-            ),
-          });
-        },
-      });
-    }
+    config.plugins.push({
+      generateBundle() {
+        this.emitFile({
+          type: 'asset',
+          fileName: 'packages/form-system/dev-assets/FormatMixinDiagram-1.svg',
+          source: fs.readFileSync(
+            path.join(__dirname, '../packages/form-system/dev-assets/FormatMixinDiagram-1.svg'),
+          ),
+        });
+        this.emitFile({
+          type: 'asset',
+          fileName: 'packages/form-system/dev-assets/FormatMixinDiagram-2.svg',
+          source: fs.readFileSync(
+            path.join(__dirname, '../packages/form-system/dev-assets/FormatMixinDiagram-2.svg'),
+          ),
+        });
+        this.emitFile({
+          type: 'asset',
+          fileName: 'packages/form-system/dev-assets/FormatMixinDiagram-3.svg',
+          source: fs.readFileSync(
+            path.join(__dirname, '../packages/form-system/dev-assets/FormatMixinDiagram-3.svg'),
+          ),
+        });
+      },
+    });
   },
 };
