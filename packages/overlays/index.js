4596d520c6b8662584ca5986f4b09b82653616be
Thomas Allmer <thomas.allmer@ing.com>
2020-05-19 09:27:07 +0200

    fix(overlays): support singleton override via setOverlays

--- packages/overlays/index.js
+++ packages/overlays/index.js
@@ -1,5 +1,5 @@
 export { globalOverlaysStyle } from './src/globalOverlaysStyle.js';
-export { overlays } from './src/overlays.js';
+export { overlays, setOverlays } from './src/overlays.js';
 export { OverlaysManager } from './src/OverlaysManager.js';
 export { OverlayController } from './src/OverlayController.js';
 export { OverlayMixin } from './src/OverlayMixin.js';
