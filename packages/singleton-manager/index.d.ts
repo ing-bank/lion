ecbe093494c4d2308a744852eb5c20f5079e6765
Joren Broekema <joren.broekema@ing.com>
2020-08-06 16:36:31 +0200

    fix(singleton-manager): add re-exports d.ts to git

---
+++ packages/singleton-manager/index.d.ts
@@ -0,0 +1,3 @@
+export { SingletonManagerClass } from "./src/SingletonManagerClass.js";
+export const singletonManager: SingletonManagerClass;
+import { SingletonManagerClass } from "./src/SingletonManagerClass.js";
