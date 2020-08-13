2d43591fde151d4b62c6a01fdad728ccc03950d9
qa46hx <Gerjan.van.Geest@ing.com>
2020-08-13 10:55:23 +0200

    feat(calendar): add year navigation

--- packages/calendar/src/utils/dayTemplate.js
+++ packages/calendar/src/utils/dayTemplate.js
@@ -14,14 +14,43 @@ const defaultMonthLabels = [
   'November',
   'December',
 ];
+const firstWeekDays = [1, 2, 3, 4, 5, 6, 7];
+const lastDaysOfYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 export function dayTemplate(day, { weekdays, monthsLabels = defaultMonthLabels } = {}) {
   const dayNumber = day.date.getDate();
   const monthName = monthsLabels[day.date.getMonth()];
   const year = day.date.getFullYear();
   const weekdayName = weekdays[day.weekOrder];
+
+  const firstDay = dayNumber === 1;
+  const endOfFirstWeek = day.weekOrder === 6 && firstWeekDays.includes(dayNumber);
+  const startOfFirstFullWeek = day.startOfWeek && firstWeekDays.includes(dayNumber);
+
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    lastDaysOfYear[1] = 29;
+  }
+  const lastDayNumber = lastDaysOfYear[day.date.getMonth()];
+  const lastWeekDays = [];
+  for (let i = lastDayNumber; i >= lastDayNumber - 7; i -= 1) {
+    lastWeekDays.push(i);
+  }
+  const endOfLastFullWeek = day.weekOrder === 6 && lastWeekDays.includes(dayNumber);
+  const startOfLastWeek = day.startOfWeek && lastWeekDays.includes(dayNumber);
+  const lastDay = lastDayNumber === dayNumber;
+
   return html`
-    <td role="gridcell" class="calendar__day-cell">
+    <td
+      role="gridcell"
+      class="calendar__day-cell"
+      ?current-month=${day.currentMonth}
+      ?first-day=${firstDay}
+      ?end-of-first-week=${endOfFirstWeek}
+      ?start-of-first-full-week=${startOfFirstFullWeek}
+      ?end-of-last-full-week=${endOfLastFullWeek}
+      ?start-of-last-week=${startOfLastWeek}
+      ?last-day=${lastDay}
+    >
       <button
         .date=${day.date}
         class="calendar__day-button"
@@ -38,7 +67,9 @@ export function dayTemplate(day, { weekdays, monthsLabels = defaultMonthLabels }
         ?current-month=${day.currentMonth}
         ?next-month=${day.nextMonth}
       >
-        ${day.date.getDate()}
+        <span class="calendar__day-button__text">
+          ${day.date.getDate()}
+        </span>
       </button>
     </td>
   `;
--- packages/calendar/translations/pl.js
+++ packages/calendar/translations/pl.js
@@ -1,4 +1,6 @@
 export default {
   nextMonth: 'Następny miesiąc',
   previousMonth: 'Poprzedni miesiąc',
+  nextFullYear: 'Następny rok',
+  previousFullYear: 'Poprzedni rok',
 };
