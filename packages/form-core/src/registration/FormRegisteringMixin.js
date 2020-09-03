aec0bdb4184671597f31a9381059bc9346e4c90e
FK62JU <tania.escudero.fernandez.ext@ing.com>
2020-09-03 15:43:41 +0200

    feat(registering): removed element

--- packages/form-core/src/registration/FormRegisteringMixin.js
+++ packages/form-core/src/registration/FormRegisteringMixin.js
@@ -1,38 +1,61 @@
 import { dedupeMixin } from '@lion/core';
 
 /**
- * @typedef {import('../../types/registration/FormRegisteringMixinTypes').FormRegisteringMixin} FormRegisteringMixin
- */
-
-/**
  * #FormRegisteringMixin:
  *
  * This Mixin registers a form element to a Registrar
  *
- * @type {FormRegisteringMixin}
+ * @polymerMixin
+ * @mixinFunction
  */
-const FormRegisteringMixinImplementation = superclass =>
-  class FormRegisteringMixin extends superclass {
-    connectedCallback() {
-      if (super.connectedCallback) {
-        super.connectedCallback();
+export const FormRegisteringMixin = dedupeMixin(
+  superclass =>
+    // eslint-disable-next-line no-shadow, no-unused-vars
+    class FormRegisteringMixin extends superclass {
+      connectedCallback() {
+        if (super.connectedCallback) {
+          super.connectedCallback();
+        }
+        this.dispatchEvent(
+          new CustomEvent('form-element-register', {
+            detail: { element: this },
+            bubbles: true,
+          }),
+        );
       }
-      this.dispatchEvent(
-        new CustomEvent('form-element-register', {
-          detail: { element: this },
-          bubbles: true,
-        }),
-      );
-    }
 
-    disconnectedCallback() {
-      if (super.disconnectedCallback) {
-        super.disconnectedCallback();
+      disconnectedCallback() {
+        if (super.disconnectedCallback) {
+          super.disconnectedCallback();
+        }
+        if (this.__parentFormGroup) {
+          this.__parentFormGroup.removeFormElement(this);
+        }
       }
-      if (this.__parentFormGroup) {
-        this.__parentFormGroup.removeFormElement(this);
-      }
-    }
-  };
 
-export const FormRegisteringMixin = dedupeMixin(FormRegisteringMixinImplementation);
+      update(changedProperties) {
+        super.update(changedProperties);
+        if (changedProperties.has('name')) {
+          const name = changedProperties.get('name');
+          if (name) {
+            this.dispatchEvent(
+              new CustomEvent('remove-form-element-register', { detail: { name }, bubbles: true }),
+            );
+          }
+        }
+      }
+
+      updated(changedProperties) {
+        super.updated(changedProperties);
+        if (changedProperties.has('name')) {
+          this.dispatchEvent(
+            new CustomEvent('form-element-register', {
+              detail: { element: this },
+              bubbles: true,
+              composed: true,
+            }),
+          );
+        }
+      }
+    },
+);
