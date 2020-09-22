4b68a9e886eae832b9f3d73f4a8d059f17253613
Rafa Bernad <rafa.bernad@outlook.com>
2020-09-22 11:01:38 +0200

    refactor: remove SingletonManager

--- packages/icon/src/icons.js
+++ packages/icon/src/icons.js
@@ -1,8 +1,7 @@
-import { singletonManager } from 'singleton-manager';
 import { IconManager } from './IconManager.js';
 
 // eslint-disable-next-line import/no-mutable-exports
-export let icons = singletonManager.get('@lion/icon::icons::0.5.x') || new IconManager();
+export let icons = new IconManager();
 
 export function setIcons(newIcons) {
   icons = newIcons;
