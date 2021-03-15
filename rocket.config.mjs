0101b7447f4783166950c86a5a71e0c63d005249
Thomas Allmer <d4kmor@gmail.com>
2021-03-15 16:27:35 +0100

    chore: set website url in netlify CI

--- rocket.config.mjs
+++ rocket.config.mjs
@@ -2,9 +2,11 @@ import { rocketLaunch } from '@rocket/launch';
 import { rocketSearch } from '@rocket/search';
 import { rocketBlog } from '@rocket/blog';
 import { adjustPluginOptions } from 'plugins-manager';
+import { absoluteBaseUrlNetlify } from '@rocket/core/helpers';
 
 export default {
   presets: [rocketLaunch(), rocketSearch(), rocketBlog()],
+  absoluteBaseUrl: absoluteBaseUrlNetlify('http://localhost:8080'),
   setupBuildPlugins: [
     adjustPluginOptions('copy', config => {
       config.patterns = [...config.patterns, 'docs/**/assets/**'];
