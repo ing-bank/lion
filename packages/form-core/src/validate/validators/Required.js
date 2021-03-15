a38470b0ed12d116051a69287e67ece80d5bb79c
Konstantinos Norgias <Konstantinos.Norgias@ing.com>
2021-03-15 12:16:06 +0100

    fix(form-core): aria-required for compatible roles

--- packages/form-core/src/validate/validators/Required.js
+++ packages/form-core/src/validate/validators/Required.js
@@ -9,6 +9,25 @@ export class Required extends Validator {
     return 'Required';
   }
 
+  static get _compatibleRoles() {
+    return [
+      'combobox',
+      'gridcell',
+      'input',
+      'listbox',
+      'radiogroup',
+      'select',
+      'spinbutton',
+      'textarea',
+      'textbox',
+      'tree',
+    ];
+  }
+
+  static get _compatibleTags() {
+    return ['input', 'select', 'textarea'];
+  }
+
   /**
    * We don't have an execute function, since the Required validator is 'special'.
    * The outcome depends on the modelValue of the FormControl and
@@ -21,7 +40,12 @@ export class Required extends Validator {
   // eslint-disable-next-line class-methods-use-this
   onFormControlConnect(formControl) {
     if (formControl._inputNode) {
-      formControl._inputNode.setAttribute('aria-required', 'true');
+      const role = formControl._inputNode.getAttribute('role') || '';
+      const elementTagName = formControl._inputNode.tagName.toLowerCase();
+      const ctor = /** @type {typeof Required} */ (this.constructor);
+      if (ctor._compatibleRoles.includes(role) || ctor._compatibleTags.includes(elementTagName)) {
+        formControl._inputNode.setAttribute('aria-required', 'true');
+      }
     }
   }
 
