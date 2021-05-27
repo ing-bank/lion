1f59caf17583f6baa2b7f74483a53c627a6998bf
Joren Broekema <joren.broekema@ing.com>
2021-05-27 17:50:12 +0200

    chore: deal with translations wildcard *

--- scripts/create-inline-lion-importmap.js
+++ scripts/create-inline-lion-importmap.js
@@ -9,32 +9,49 @@ const fs = promises;
 async function main() {
   let importMaps = {};
 
+  // Get all paths to package.jsons inside our monorepo
   const pkgJsonPaths = await globby(rootPkgJson.workspaces.map(glob => `${glob}/package.json`));
 
-  pkgJsonPaths.forEach(pkgJsonPath => {
-    // TODO: use fs maybe... since it complains about non-global + dynamic require usage
+  for (const pkgJsonPath of pkgJsonPaths) {
     // eslint-disable-next-line import/no-dynamic-require, global-require
     const json = require(path.resolve(pkgJsonPath));
 
     const fullPkgName = json.name;
     const pkgName = fullPkgName.split('@lion')[1];
-    if (!pkgName) {
-      return;
+
+    if (pkgName) {
+      const mappedExports = {};
+      const { exports } = json;
+
+      // Go through all exports and start mapping
+      const entries = Object.entries(exports);
+      for (const entry of entries) {
+        const key = entry[0].replace('.', fullPkgName);
+        const value = entry[1].replace('.', `./lion/packages${pkgName}`);
+
+        // Expand on singular wildcard *
+        // Assumptions: it's at the end of the string, it's only 1
+        if (key.match(/\*/g)) {
+          const absoluteTranslationsPath = path.resolve(
+            pkgJsonPath.split('/package.json')[0],
+            entry[0],
+          );
+          const filesFromWildcard = await globby(absoluteTranslationsPath);
+
+          for (const filePath of filesFromWildcard.map(_filePath => path.basename(_filePath))) {
+            mappedExports[key.replace('*', filePath)] = value.replace('*', filePath);
+          }
+        } else {
+          mappedExports[key] = value;
+        }
+      }
+
+      importMaps = {
+        ...importMaps,
+        ...mappedExports,
+      };
     }
-    const { exports } = json;
-
-    const mappedExports = {};
-    Object.entries(exports).forEach(entry => {
-      const key = entry[0].replace('.', fullPkgName);
-      const value = entry[1].replace('.', `./lion/packages${pkgName}`);
-      mappedExports[key] = value;
-    });
-
-    importMaps = {
-      ...importMaps,
-      ...mappedExports,
-    };
-  });
+  }
 
   return importMaps;
 }
