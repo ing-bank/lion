494643e2cf55f3798f8b958b32cf12fd47c9a669
Thomas Allmer <d4kmor@gmail.com>
2021-05-31 14:53:44 +0200

    chore: remove generated type files from git

--- packages-node/publish-docs/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { PublishDocs } from "./src/PublishDocs.js";
-export type PublishDocsOptions = {
-    projectDir: string;
-    gitHubUrl: string;
-    gitRootDir: string;
-    copyDir: string;
-    copyTarget: string;
-};
