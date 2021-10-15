e1ab99a5f1d4a30f36e6c01dcee7c2fbde87f51f
Thomas Allmer <thomas.allmer@ing.com>
2021-10-15 22:22:30 +0200

    chore: some more fixes

--- rocket.config.mjs
+++ rocket.config.mjs
@@ -1,8 +1,9 @@
 import { rocketLaunch } from '@rocket/launch';
 import { rocketSearch } from '@rocket/search';
 import { rocketBlog } from '@rocket/blog';
-import { absoluteBaseUrlNetlify } from '@rocket/core';
-import { adjustPluginOptions, mdjsSetupCode } from 'plugins-manager';
+import { absoluteBaseUrlNetlify } from '@rocket/core/helpers';
+import { adjustPluginOptions } from 'plugins-manager';
+import { mdjsSetupCode } from '@mdjs/core';
 import { copy } from '@web/rollup-plugin-copy';
 
 export default {
