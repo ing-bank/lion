9b81b69e58c6495a86c8a56d33a71a141c6fe617
jorenbroekema <joren.broekema@gmail.com>
2021-11-09 14:46:00 +0100

    feat(core): export latest added lit directives

--- packages/core/index.js
+++ packages/core/index.js
@@ -55,9 +55,13 @@ export { cache } from 'lit/directives/cache.js';
 export { classMap } from 'lit/directives/class-map.js';
 export { guard } from 'lit/directives/guard.js';
 export { ifDefined } from 'lit/directives/if-defined.js';
+export { live } from 'lit/directives/live.js';
+export { ref, createRef } from 'lit/directives/ref.js';
 export { repeat } from 'lit/directives/repeat.js';
 export { styleMap } from 'lit/directives/style-map.js';
+export { templateContent } from 'lit/directives/template-content.js';
 export { unsafeHTML } from 'lit/directives/unsafe-html.js';
+export { unsafeSVG } from 'lit/directives/unsafe-svg.js';
 export { until } from 'lit/directives/until.js';
 
 // open-wc
