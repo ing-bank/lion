e9c2bf01a5b7d12e291547299128838965d8bcc8
Mikko De Vera <me@devmikko.com>
2022-03-08 16:11:28 +0800

    fix(lang): Fix default en-PH date validation message

--- packages/validate-messages/src/loadDefaultFeedbackMessages.js
+++ packages/validate-messages/src/loadDefaultFeedbackMessages.js
@@ -61,6 +61,7 @@ export function loadDefaultFeedbackMessages() {
             case 'en-US':
               return import('@lion/validate-messages/translations/en-US.js');
             case 'en-PH':
+              return import('@lion/validate-messages/translations/en-PH.js');
             case 'en':
               return import('@lion/validate-messages/translations/en.js');
             case 'es-ES':
