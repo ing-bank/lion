e9c6523f59975d4f8e2f0360fe55d0cc7a38b653
Thijs Louisse <Thijs.Louisse@ing.com>
2022-09-14 17:03:01 +0200

    feat(providence-analytics): export more and use cjs only

---
+++ packages-node/providence-analytics/src/program/utils/index.js
@@ -0,0 +1,9 @@
+const {
+  getSourceCodeFragmentOfDeclaration,
+} = require('./get-source-code-fragment-of-declaration.js');
+
+// TODO: move trackdownIdentifier to utils as well
+
+module.exports = {
+  getSourceCodeFragmentOfDeclaration,
+};
