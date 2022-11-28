f0e6ee922c20c6cd516740d80d1122c0ad18f36a
Thijs Louisse <Thijs.Louisse@ing.com>
2022-11-28 17:36:03 +0100

    feat(ui): export side effect free singletons for deduping multiple instances before "main" singleton instance is registered

--- packages/ui/components/overlays/src/singleton.js
+++ packages/ui/components/overlays/src/singleton.js
@@ -1,12 +1,4 @@
 import { singletonManager } from 'singleton-manager';
 import { OverlaysManager } from './OverlaysManager.js';
 
-// eslint-disable-next-line import/no-mutable-exports
-export let overlays = singletonManager.get('@lion/ui::overlays::0.x') || new OverlaysManager();
-
-/**
- * @param {OverlaysManager} newOverlays
- */
-export function setOverlays(newOverlays) {
-  overlays = newOverlays;
-}
+export const overlays = singletonManager.get('@lion/ui::overlays::0.x') || new OverlaysManager();
--- packages/ui/exports/icon.js
+++ packages/ui/exports/icon.js
@@ -1,3 +1,3 @@
 export { LionIcon } from '../components/icon/src/LionIcon.js';
 export { IconManager } from '../components/icon/src/IconManager.js';
-export { icons, setIcons } from '../components/icon/src/icons.js';
+export { icons } from '../components/icon/src/icons.js';
