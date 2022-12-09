569b9d7edfbef21b221909a936a897348b05aa48
Thijs Louisse <Thijs.Louisse@ing.com>
2022-12-09 16:09:42 +0100

    chore: copy overlays docs assets inside ui pkg

--- packages/ui/docs/fundamentals/systems/overlays/assets/applyDemoOverlayStyles.mjs
+++ packages/ui/docs/fundamentals/systems/overlays/assets/applyDemoOverlayStyles.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { css } from 'lit';
 
 const applyDemoOverlayStyles = () => {
--- packages/ui/docs/fundamentals/systems/overlays/assets/demo-overlay-backdrop.mjs
+++ packages/ui/docs/fundamentals/systems/overlays/assets/demo-overlay-backdrop.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { css, LitElement } from 'lit';
 
 /**
