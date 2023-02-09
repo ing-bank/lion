9ff7cd778dc45bf7b141aa1a8d4a39f7dd767e91
Riovir <riovir.dev@gmail.com>
2023-02-09 10:49:00 +0100

    fix(@lion/ui): export types necessary for type inference of mixins

--- packages/ui/exports/types/core.ts
+++ packages/ui/exports/types/core.ts
@@ -1,1 +1,4 @@
 export { SlotHost } from '../../components/core/types/SlotMixinTypes.js';
+export { SlotsMap } from '../../components/core/types/SlotMixinTypes.js';
+export { SlotFunctionResult } from '../../components/core/types/SlotMixinTypes.js';
+export { SlotRerenderObject } from '../../components/core/types/SlotMixinTypes.js';
