e871fe67fc9aab82d06d81604938d2b5cb49c1d4
Riovir <riovir.dev@gmail.com>
2023-02-10 14:57:09 +0100

    fix(@lion/ui): export types necessary for type inference of core.js

--- packages/ui/exports/types/core.ts
+++ packages/ui/exports/types/core.ts
@@ -1,3 +1,5 @@
+export { DisabledHost } from '../../components/core/types/DisabledMixinTypes.js';
+export { DisabledWithTabIndexHost } from '../../components/core/types/DisabledWithTabIndexMixinTypes.js';
 export { SlotHost } from '../../components/core/types/SlotMixinTypes.js';
 export { SlotsMap } from '../../components/core/types/SlotMixinTypes.js';
 export { SlotFunctionResult } from '../../components/core/types/SlotMixinTypes.js';
