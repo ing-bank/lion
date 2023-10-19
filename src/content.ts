73c5f4686d82e73aaf29698bed922da7f69052ab
okadurin <ovkadurin@gmail.com>
2023-10-19 15:35:54 +0200

    Add guides, blog, fundamentals

--- src/content.ts
+++ src/content.ts
@@ -1,12 +1,18 @@
 import { getCollection } from 'astro:content';
 import {
+  isBlogEntry,
 	isComponentInfoEntry,
-  isComponentPageEntry
+  isComponentPageEntry,
+  isFundamentalsEntry,
+  isGuideEntry
 } from './content/config';
 
 export const allPages = await getCollection('docs');
-export const componentInfoEntries = allPages.filter(isComponentInfoEntry)    
+export const componentInfoEntries = allPages.filter(isComponentInfoEntry);
 export const componentPageEntries = allPages.filter(isComponentPageEntry);
+export const blogEntries = allPages.filter(isBlogEntry);
+export const guideEntries = allPages.filter(isGuideEntry);
+export const fundamentalsEntries = allPages.filter(isFundamentalsEntry);
 export const getInfoParentSlug = entry => entry.slug.split('/info')[0];
 
 
