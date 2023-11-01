4c157f866a1ddf007c32bc355243f3a9bff4ae9b
okadurin <ovkadurin@gmail.com>
2023-11-01 14:56:54 +0100

    chore: update script so that it creates info.md

--- src/utils/copy-docs.mjs
+++ src/utils/copy-docs.mjs
@@ -5,10 +5,25 @@ const sourceDocsPath = 'docs';
 const contentDocsPath = 'contentDocs';
 const publicDocsPath = 'publicDocs';
 
-const createMdFrontmatter = (componentName) => {    
+const createComponentMdFrontmatter = (componentName) => {    
     return `---
 component: ${componentName}
+---\n\n`;
+}
+
+async function createInfoMd(componentDirectoryPath) {
+    console.log('componentDirectoryPath: ', componentDirectoryPath)
+    const componentName = path.basename(componentDirectoryPath);
+    const infoMd = `---
+component: ${componentName}
+title: ${componentName} title
+description: ${componentName} description    
 ---\n`;
+    const infoMdFilePath = path.join(componentDirectoryPath, 'info.md');
+    console.log('infoMdFilePath: ', infoMdFilePath)
+    console.log('infoMdFilePath: ', infoMdFilePath)
+    await fs.mkdir(componentDirectoryPath, { recursive: true });
+    await fs.writeFile(infoMdFilePath, infoMd);
 }
 
 async function copyDocs(currentPath = '') {    
@@ -21,6 +36,12 @@ async function copyDocs(currentPath = '') {
         const stats = await fs.lstat(sourceDocsFilePath);
 
         if (stats.isDirectory()) {
+            console.log('sourceDocsFilePath1: ', sourceDocsFilePath);
+            console.log('file1: ', file);
+            console.log('currentPath1: ', currentPath);
+            if (currentPath === 'components') { 
+                await createInfoMd(contentDocsFilePath);
+            }
             await copyDocs(path.join(currentPath, file));
         } else {
             await fs.mkdir(path.join(contentDocsPath, currentPath), { recursive: true });
@@ -30,7 +51,7 @@ async function copyDocs(currentPath = '') {
                 const parentComponent = path.basename(currentPath);
                 console.log(`currentPath: ${currentPath}, parentComponent: ${parentComponent}`);
                 const fileContent = await fs.readFile(contentDocsFilePath, 'utf8');
-                const updatedFileContent = `${createMdFrontmatter(parentComponent)}${fileContent}`;
+                const updatedFileContent = `${createComponentMdFrontmatter(parentComponent)}${fileContent}`;
                 await fs.writeFile(contentDocsFilePath, updatedFileContent);
             }
             if (path.extname(file) !== '.md') {
