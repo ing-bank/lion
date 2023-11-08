30658f91be17efd1b70725809f8da6e10b73bfe4
okadurin <ovkadurin@gmail.com>
2023-11-08 11:10:39 +0100

    chore: remove roket metadata from md files

---
+++ src/utils/remark-plugings/cleanupRocketMetadata/cleanupRocketMetadata.js
@@ -0,0 +1,49 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { init } = require('es-module-lexer');
+
+let visit;
+(async () => {
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  const result = await import('unist-util-visit');
+  visit = result.visit;
+})();
+
+function cleanupRocketMetadata() {
+  /**
+   * @param {Node} tree
+   */
+  async function transformer(tree) {
+    /**
+     * @param {UnistNode} _node
+     */
+    async function nodeCodeVisitor(_node, index, parent) {
+      if (parent.type === 'heading') {
+        if (parent.depth === 1) {
+          const splitByOrder = _node.value.split('||');
+          const order = splitByOrder[1].trim();
+          if (!order) {
+            return;
+          }
+          const splitByArrows = splitByOrder[0].split('>>');
+          const title = splitByArrows[splitByArrows.length - 1].trim();
+          // eslint-disable-next-line no-param-reassign
+          _node.value = title;
+        }
+        // eslint-disable-next-line no-param-reassign
+        parent.depth += 1;
+      }
+    }
+
+    // unifiedjs expects node changes to be made on the given node...
+    await init;
+    visit(tree, 'text', nodeCodeVisitor);
+
+    return tree;
+  }
+
+  return transformer;
+}
+
+module.exports = {
+  cleanupRocketMetadata,
+};
