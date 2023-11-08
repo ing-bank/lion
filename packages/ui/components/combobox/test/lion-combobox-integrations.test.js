ee4ea16d48858884e3110f6ef0b9b532e4c20adc
Thijs Louisse <t_louisse@hotmail.com>
2023-11-08 13:10:16 +0100

    chore: fix lint

--- packages/ui/components/combobox/test/lion-combobox-integrations.test.js
+++ packages/ui/components/combobox/test/lion-combobox-integrations.test.js
@@ -1,6 +1,6 @@
 import { runListboxMixinSuite } from '@lion/ui/listbox-test-suites.js';
-import { runCustomChoiceGroupMixinSuite } from '../../form-core/test-suites/choice-group/CustomChoiceGroupMixin.suite.js';
 import '@lion/ui/define/lion-combobox.js';
+import { runCustomChoiceGroupMixinSuite } from '../../form-core/test-suites/choice-group/CustomChoiceGroupMixin.suite.js';
 
 runListboxMixinSuite({ tagString: 'lion-combobox' });
 runCustomChoiceGroupMixinSuite({
