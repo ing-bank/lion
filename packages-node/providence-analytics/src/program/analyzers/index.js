cf6d352359939c33c451650b9dae0f90512c1e28
Thijs Louisse <Thijs.Louisse@ing.com>
2023-11-29 12:39:28 +0100

    fix(providence): correct export path for MatchImportAnalyzer

--- packages-node/providence-analytics/src/program/analyzers/index.js
+++ packages-node/providence-analytics/src/program/analyzers/index.js
@@ -4,7 +4,7 @@ export { Analyzer } from '../core/Analyzer.js';
 // Expose analyzers that are requested to be run in external contexts
 export { default as FindExportsAnalyzer } from './find-exports.js';
 export { default as FindImportsAnalyzer } from './find-imports.js';
-export { default as MatchImportsAnalyzer } from './match-paths.js';
+export { default as MatchImportsAnalyzer } from './match-imports.js';
 
 export { transformIntoIterableFindImportsOutput } from './helpers/transform-into-iterable-find-imports-output.js';
 export { transformIntoIterableFindExportsOutput } from './helpers/transform-into-iterable-find-exports-output.js';
