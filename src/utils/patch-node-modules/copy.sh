9e8bb07891a5173639199958a284da6f22140ba8
okadurin <ovkadurin@gmail.com>
2024-01-09 16:32:37 +0100

    chore: update dependencies in node_modules

---
+++ src/utils/patch-node-modules/copy.sh
@@ -0,0 +1,3 @@
+cp src/utils/patch-node-modules/@lion/core@0.19.0/index.js node_modules/@lion/overlays/node_modules/@lion/core/index.js
+cp src/utils/patch-node-modules/@mdjs/mdjs-preview@0.5.9/src/MdJsPreview.js node_modules/@mdjs/mdjs-preview/src/MdJsPreview.js
+cp src/utils/patch-node-modules/@lion/core@0.22.0/index.js node_modules/@lion/core/index.js
