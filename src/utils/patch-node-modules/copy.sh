0b631457f5a92eea52c69327528a91155b991a9e
okadurin <ovkadurin@gmail.com>
2024-01-17 17:35:41 +0100

    chore: remove copy.sh

--- src/utils/patch-node-modules/copy.sh
+++
@@ -1,5 +0,0 @@
-cp src/utils/patch-node-modules/@lion/core@0.19.0/index.js node_modules/@lion/overlays/node_modules/@lion/core/index.js
-cp src/utils/patch-node-modules/@mdjs/mdjs-preview@0.5.9/src/MdJsPreview.js node_modules/@mdjs/mdjs-preview/src/MdJsPreview.js
-cp src/utils/patch-node-modules/@lion/core@0.22.0/index.js node_modules/@lion/core/index.js
-
-cp src/utils/patch-node-modules/@open-wc/scoped-elements/html-element.js node_modules/@open-wc/scoped-elements/html-element.js
