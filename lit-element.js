f6e2f49bfdac4a35bbc4a313c4036b02d331e91d
okadurin <ovkadurin@gmail.com>
2024-01-22 14:20:04 +0100

    chore: update lit-element.js

---
+++ lit-element.js
@@ -0,0 +1,53 @@
+import { dedupeMixin } from '@open-wc/dedupe-mixin';
+import { adoptStyles } from 'lit';
+import { ScopedElementsMixin as BaseScopedElementsMixin } from './html-element.js';
+
+/**
+ * @typedef {import('./types.js').ScopedElementsHost} ScopedElementsHost
+ * @typedef {import('./types.js').ScopedElementsMap} ScopedElementsMap
+ * @typedef {import('lit').CSSResultOrNative} CSSResultOrNative
+ * @typedef {import('lit').LitElement} LitElement
+ * @typedef {typeof import('lit').LitElement} TypeofLitElement
+ * @typedef {import('@open-wc/dedupe-mixin').Constructor<LitElement>} LitElementConstructor
+ * @typedef {import('@open-wc/dedupe-mixin').Constructor<ScopedElementsHost>} ScopedElementsHostConstructor
+ */
+
+// @ts-ignore
+const supportsScopedRegistry = !!ShadowRoot.prototype.createElement;
+
+/**
+ * @template {LitElementConstructor} T
+ * @param {T} superclass
+ * @return {T & ScopedElementsHostConstructor}
+ */
+const ScopedElementsMixinImplementation = superclass =>
+  /** @type {ScopedElementsHost} */
+  class ScopedElementsHost extends BaseScopedElementsMixin(superclass) {
+    createRenderRoot() {
+      const { shadowRootOptions, elementStyles } = /** @type {TypeofLitElement} */ (
+        this.constructor
+      );      
+
+      //const shadowRoot = this.attachShadow(shadowRootOptions);
+      const createdRoot = this.attachShadow(shadowRootOptions);
+      if (supportsScopedRegistry) {
+        // @ts-ignore
+        this.renderOptions.creationScope = createdRoot;
+      }
+      // // @ts-ignore
+      // this.renderOptions.creationScope = shadowRoot;
+
+      if (createdRoot instanceof ShadowRoot) {
+        adoptStyles(createdRoot, elementStyles);
+        this.renderOptions.renderBefore = this.renderOptions.renderBefore || createdRoot.firstChild;
+      }
+
+      // adoptStyles(shadowRoot, elementStyles);
+
+      // this.renderOptions.renderBefore ??= shadowRoot.firstChild;
+
+      return createdRoot;
+    }
+  };
+
+export const ScopedElementsMixin = dedupeMixin(ScopedElementsMixinImplementation);
