a3ca59956623ea88f100387879cfad5f9c8f5682
Thijs Louisse <Thijs.Louisse@ing.com>
2024-04-09 19:57:15 +0200

    feat(ui): [core] allow browserDetection to be run in ssr context

---
+++ .changeset/thick-worms-grab.md
@@ -0,0 +1,5 @@
+---
+'@lion/ui': patch
+---
+
+[core] allow browserDetection to be run in ssr context
--- .eslintrc.js
+++ .eslintrc.js
@@ -48,4 +48,7 @@ module.exports = {
       [path.resolve('./scripts/eslint-resolver.cjs')]: {},
     },
   },
+  env: {
+    es2020: true,
+  },
 };
