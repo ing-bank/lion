0f4947785fc328830558e71359e5f624977bbc98
gerjanvangeest <gerjanvangeest@users.noreply.github.com>
2024-04-10 11:09:52 +0200

    Update packages/ui/components/input-amount/test/parsers.test.js
    Co-authored-by: Thijs Louisse <thijs.louisse@ing.com>

--- packages/ui/components/input-amount/test/parsers.test.js
+++ packages/ui/components/input-amount/test/parsers.test.js
@@ -49,7 +49,7 @@ describe('parseAmount()', async () => {
     expect(parseAmount('12,345,678,987,654,321.42')).to.equal(12345678987654321.42);
   });
 
-  it('returns undefined if a invalid value is entered', async () => {
+  it('returns undefined if an invalid value is entered', async () => {
     expect(parseAmount('foo')).to.equal(undefined);
     expect(parseAmount('foo1')).to.equal(undefined);
     expect(parseAmount('EUR 1,50')).to.equal(undefined);
