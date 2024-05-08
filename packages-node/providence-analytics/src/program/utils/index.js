9289e7652e034035c6353eb7867ad35472bbc56d
Thijs Louisse <Thijs.Louisse@ing.com>
2024-05-08 14:17:04 +0200

    feat: expose fs-adapter and optimised-glob

--- packages-node/providence-analytics/src/program/utils/index.js
+++ packages-node/providence-analytics/src/program/utils/index.js
@@ -1,8 +1,9 @@
+export { toRelativeSourcePath, isRelativeSourcePath } from './relative-source-path.js';
 export {
   getSourceCodeFragmentOfDeclaration,
   getFilePathOrExternalSource,
 } from './get-source-code-fragment-of-declaration.js';
+export { optimisedGlob } from './optimised-glob.js';
+export { fsAdapter } from './fs-adapter.js';
 export { memoize } from './memoize.js';
-export { toRelativeSourcePath, isRelativeSourcePath } from './relative-source-path.js';
-
 // TODO: move trackdownIdentifier to utils as well
