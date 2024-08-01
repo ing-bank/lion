58796deb0e9798c679ab44d01748c309b4492316
Oleksii Kadurin <ovkadurin@gmail.com>
2024-08-01 18:55:36 +0200

    fix: add slot support to deepContains function

--- packages/ui/components/overlays/test/utils-tests/deep-contains.test.js
+++ packages/ui/components/overlays/test/utils-tests/deep-contains.test.js
@@ -124,4 +124,102 @@ describe('deepContains()', () => {
     expect(deepContains(element, elementFirstChildShadowChildShadow)).to.be.true;
     expect(deepContains(element, elementFirstChildShadowChildShadowLastChild)).to.be.true;
   });
+
+  it('returns true if the element, which is located in ShadowsRoot, contains a target element, located in the LightDom', async () => {
+    const mainElement = /** @type {HTMLElement} */ (await fixture('<div id="main"></div>'));
+    mainElement.innerHTML = `      
+      <div slot="content" id="light-el-content">
+        <input type="text" id="light-el-input-1"></input>
+      </div>
+      <div slot="content" id="light-el-content">
+        <input type="text" id="light-el-input-2"></input>
+      </div>
+    `;
+    const shadowRoot = mainElement.attachShadow({ mode: 'open' });
+    shadowRoot.innerHTML = `
+      <div id="dialog-wrapper">
+        <div id="dialog-header">
+          Header          
+        </div>  
+        <div id="dialog-content">
+          <slot name="content" id="shadow-el-content"></slot>
+        </div>  
+      </div>      
+    `;
+    const inputElement = /** @type {HTMLElement} */ (
+      mainElement.querySelector('#light-el-input-2')
+    );
+    const dialogWrapperElement = /** @type {HTMLElement} */ (
+      shadowRoot.querySelector('#dialog-wrapper')
+    );
+    expect(deepContains(dialogWrapperElement, inputElement)).to.be.true;
+  });
+
+  it(`returns true if the element, which is located in ShadowRoot, contains a target element, located in the ShadowRoot element of the LightDom element  `, async () => {
+    /**
+     * The DOM for the `main` element looks as follows:
+     *
+     * <div id="main">
+     *   #shadow-root
+     *     <div id="dialog-wrapper">                                        // dialogWrapperElement
+     *       <div id="dialog-header">
+     *         Header
+     *       </div>
+     *       <div id="dialog-content">
+     *         <slot name="content" id="shadow-el-content"></slot>
+     *       </div>
+     *     </div>
+     *   <div slot="content" id="light-el-content">
+     *     <div id="conent-wrapper">
+     *       #shadow-root
+     *         <div id="conent-wrapper-sub">
+     *           #shadow-root
+     *             <input type="type" id="content-input"></input>           //inputElement
+     *         </div>
+     *     </div>
+     *   </div>
+     * </div>
+     */
+    const mainElement = /** @type {HTMLElement} */ (await fixture('<div id="main"></div>'));
+    mainElement.innerHTML = `      
+      <div slot="content" id="light-el-content">
+        <div id="content-wrapper"></div>
+      </div>
+      <div slot="content" id="light-el-content">
+        <div id="content-wrapper"></div>
+      </div>
+    `;
+    const contentWrapper = /** @type {HTMLElement} */ (
+      mainElement.querySelector('#content-wrapper')
+    );
+    const contentWrapperShadowRoot = contentWrapper.attachShadow({ mode: 'open' });
+    contentWrapperShadowRoot.innerHTML = `
+      <div id="conent-wrapper-sub"></div>
+    `;
+    const contentWrapperSub = /** @type {HTMLElement} */ (
+      contentWrapperShadowRoot.querySelector('#conent-wrapper-sub')
+    );
+    const contentWrapperSubShadowRoot = contentWrapperSub.attachShadow({ mode: 'open' });
+    contentWrapperSubShadowRoot.innerHTML = `
+      <input type="type" id="content-input"></input>
+    `;
+    const inputElement = /** @type {HTMLElement} */ (
+      contentWrapperSubShadowRoot.querySelector('#content-input')
+    );
+    const mainElementShadowRoot = mainElement.attachShadow({ mode: 'open' });
+    mainElementShadowRoot.innerHTML = `
+      <div id="dialog-wrapper">
+        <div id="dialog-header">
+          Header          
+        </div>  
+        <div id="dialog-content">
+          <slot name="content" id="shadow-el-content"></slot>
+        </div>  
+      </div>      
+    `;
+    const dialogWrapperElement = /** @type {HTMLElement} */ (
+      mainElementShadowRoot.querySelector('#dialog-wrapper')
+    );
+    expect(deepContains(dialogWrapperElement, inputElement)).to.be.true;
+  });
 });
