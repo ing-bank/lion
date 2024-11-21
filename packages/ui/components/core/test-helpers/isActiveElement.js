1c0f3450802a660af5230bd89a879d3e8c507e52
Thijs Louisse <thijs.louisse@ing.com>
2024-11-21 09:00:17 +0100

    Update packages/ui/components/core/test-helpers/isActiveElement.js
    Co-authored-by: gerjanvangeest <gerjanvangeest@users.noreply.github.com>

--- packages/ui/components/core/test-helpers/isActiveElement.js
+++ packages/ui/components/core/test-helpers/isActiveElement.js
@@ -1,7 +1,7 @@
 import { getDeepActiveElement } from '../src/getDeepActiveElement.js';
 /**
  * Readable alternative for `expect(el).to.equal(document.activeElement);`.
- * While this is readable by itself, it makes Web Test Runner hang completelt in many occasions.
+ * While this is readable by itself, it makes Web Test Runner hang completely in many occasions.
  * Therefore it's better to write:
  * `expect(isActiveElement(el)).to.be.true;`
  * @param {Element} el
