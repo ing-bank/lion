3b663ca98d4b843bc9a5315a57c9e16c5195d8b2
Thijs Louisse <Thijs.Louisse@ing.com>
2024-12-18 19:34:12 +0100

    fix(overlays): overlayController teardown cleanup

--- packages/ui/components/overlays/types/ArrowMixinTypes.ts
+++ packages/ui/components/overlays/types/ArrowMixinTypes.ts
@@ -4,25 +4,34 @@ import { Options as PopperOptions, State } from '@popperjs/core/lib/popper.js';
 import { OverlayConfig } from './OverlayConfig.js';
 
 export declare class ArrowHost {
-  static get properties(): {
+  static properties: {
     hasArrow: {
       type: BooleanConstructor;
       reflect: boolean;
       attribute: string;
     };
   };
+
   hasArrow: boolean;
+
   repositionComplete: Promise<void>;
 
-  static get styles(): CSSResultArray;
+  static styles: CSSResultArray;
 
   render(): TemplateResult;
+
   protected _arrowTemplate(): TemplateResult;
+
   protected _arrowNodeTemplate(): TemplateResult;
+
   protected _defineOverlayConfig(): OverlayConfig;
+
   protected _getPopperArrowConfig(popperConfigToExtendFrom: Partial<PopperOptions>): Partial<PopperOptions>;
+
   private __setupRepositionCompletePromise(): void;
+
   get _arrowNode(): Element | null;
+
   private __syncFromPopperState(data: Partial<State>): void;
 }
 
