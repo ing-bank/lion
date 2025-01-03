fe5fac201940a8f94d06ea2bda78024c2d8b096b
Vladimir Skorokhod <vaskorohod@gmail.com>
2025-01-03 14:09:44 +0100

    update getParseMode to set mode auto by default

--- packages/ui/components/localize/src/number/parseNumber.js
+++ packages/ui/components/localize/src/number/parseNumber.js
@@ -16,19 +16,19 @@ import { getDecimalSeparator } from './getDecimalSeparator.js';
  * parseNumber('1,234.56'); // method: heuristic => 1234.56
  *
  * @param {string} value Clean number (only [0-9 ,.]) to be parsed
- * @param {import('../../types/LocalizeMixinTypes.js').FormatNumberOptions} [options] Locale Options
+ * @param {import('../../types/LocalizeMixinTypes.js').FormatNumberOptions} options Locale Options
  * @return {string} unparseable|withLocale|heuristic
  */
-function getParseMode(value, options = { mode: 'auto' }) {
+function getParseMode(value, { mode = 'auto', locale }) {
   const separators = value.match(/[., ]/g);
 
   if (!separators) {
     return 'withLocale';
   }
-  if (options.locale) {
+  if (locale) {
     return 'withLocale';
   }
-  if (options.mode === 'auto' && separators.length === 1) {
+  if (mode === 'auto' && separators.length === 1) {
     const decimalLength = value.split(`${separators}`)[1].length;
     if (decimalLength >= 3) {
       return 'withLocale';
