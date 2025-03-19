9827228b83b1bd54f2def383a573d5e22a906f65
Thijs Louisse <Thijs.Louisse@ing.com>
2025-03-19 23:54:36 +0100

    fix(providence): use globby again

--- packages-node/providence-analytics/src/program/utils/index.js
+++ packages-node/providence-analytics/src/program/utils/index.js
@@ -4,7 +4,6 @@ export {
   getSourceCodeFragmentOfDeclaration,
   getFilePathOrExternalSource,
 } from './get-source-code-fragment-of-declaration.js';
-export { optimisedGlob } from './optimised-glob.js';
 export { oxcTraverse } from './oxc-traverse.js';
 export { fsAdapter } from './fs-adapter.js';
 export { memoize } from './memoize.js';
