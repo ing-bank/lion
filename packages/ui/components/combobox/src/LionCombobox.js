c95457b77a40bc960a0696985b0470d3bb519e8e
JYOTHI <JYOTHI@Jyomayi.local>
2025-04-02 12:52:08 +0530

    fix: preselection in combobox

--- packages/ui/components/combobox/src/LionCombobox.js
+++ packages/ui/components/combobox/src/LionCombobox.js
@@ -1175,7 +1175,10 @@ export class LionCombobox extends LocalizeMixin(OverlayMixin(CustomChoiceGroupMi
   // eslint-disable-next-line no-unused-vars
   _syncToTextboxCondition(modelValue, oldModelValue, { phase } = {}) {
     return (
-      this.autocomplete === 'inline' || this.autocomplete === 'both' || phase === 'overlay-close'
+      modelValue !== oldModelValue ||
+      this.autocomplete === 'inline' ||
+      this.autocomplete === 'both' ||
+      phase === 'overlay-close'
     );
   }
 
