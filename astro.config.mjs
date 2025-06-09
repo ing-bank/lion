a85165bea30870fe86b438876ae44cef856c2de7
Pavlik Kiselev <pavlik.kiselev@ing.com>
2025-06-09 16:22:28 +0200

    feat: improved Blog sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
-import { defineConfig } from "astro/config";
-import lit from "@astrojs/lit";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { globby } from 'globby';
+import lit from '@astrojs/lit';
+import { defineConfig } from 'astro/config';
 import { mdjsParse, mdjsStoryParse, mdjsSetupCode } from '@mdjs/core';
 import { copyMdjsStories } from './src/utils/remark-plugings/copyMdjsStories/index.js';
-import { cleanupRocketMetadata } from './src/utils/remark-plugings/cleanupRocketMetadata/index.js';
-import { updateMainTagsForMdjsStories } from './src/utils/remark-plugings/updateMainTagsForMdjsStories/index.js';
+import { remarkModifiedTime } from './src/utils/remark-plugings/remark-modified-time.mjs';
+
+
+const publicJsFiles = await globby('public/**/*.js');
+const publicMdFilesObj = Object.fromEntries(
+  publicJsFiles.map(file => [
+    // This remove `src/` as well as the file extension from each
+    // file, so e.g. src/nested/foo.js becomes nested/foo
+    path.relative('public', file.slice(0, file.length - path.extname(file).length)),
+    // This expands the relative paths to absolute paths, so e.g.
+    // src/nested/foo becomes /project/src/nested/foo.js
+    fileURLToPath(new URL(file, import.meta.url)),
+  ]),
+);
+
+console.log('publicMdFilesObj: ', publicMdFilesObj);
 
 const mdjsSetupConfig = {
   simulationSettings: {
@@ -17,11 +34,34 @@ const mdjsSetupConfig = {
   },
 };
 
+console.log('mode', import.meta.env.MODE);
+
 // https://astro.build/config
 export default defineConfig({
+  // redirect /components/accordion to /components/accordion/overview
+  // this is done in Lion via index.md: -> go to Overview
+  redirects: {
+    '/components/[component]': '/components/[component]/overview',
+  },
   integrations: [lit()],
-  markdown: {  
-    remarkPlugins: [updateMainTagsForMdjsStories, mdjsParse, mdjsStoryParse, [mdjsSetupCode, mdjsSetupConfig], copyMdjsStories, cleanupRocketMetadata],
+  markdown: {
+    // ing-web
+    remarkPlugins: [
+      /*...extendLionDocsInstance,*/ mdjsParse,
+      mdjsStoryParse,
+      [mdjsSetupCode, mdjsSetupConfig],
+      [copyMdjsStories, { mode: import.meta.env.MODE }],
+      remarkModifiedTime,
+      // cleanRocketMarkdown,
+    ],
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    },
+    // lion
+    //remarkPlugins: [mdjsParse, mdjsStoryParse, [mdjsSetupCode, mdjsSetupConfig], copyMdjsStories],
   },
   vite: {
     // the fix is copied from https://github.com/withastro/astro/issues/5517#issuecomment-1337328843.
@@ -32,8 +72,11 @@ export default defineConfig({
     // 6 │ await init;
     // ```
     optimizeDeps: {
-      exclude: ['rocket-preset-extend-lion-docs']
+      // exclude: ['rocket-preset-extend-lion-docs'],
+      noDiscovery: true,
+      include: [],
     },
+
     // Fix taken from https://github.com/vitejs/vite/issues/6985#issuecomment-1044375490.
     // It throws an error otherwise:
     // ```
@@ -44,10 +87,34 @@ export default defineConfig({
     // build: {
     //   target: 'esnext'
     // },
+
     build: {
       rollupOptions: {
-        external: /^lit/
-      }
-    }
+        input: publicMdFilesObj,
+        // output: {
+        //   dir: 'kist',
+        //   format: 'esm',
+        //   entryFileNames: '[name]/__mdjs-stories.js',
+        // },
+        plugins: [
+          {
+            name: 'q-d',
+            resolveId: importee => {
+              console.log('importee: ', importee);
+              return null;
+            },
+            load(id) {
+              console.log('id: ', id);
+
+              if (id === 'virtual-module') {
+                // the source code for "virtual-module"
+                return 'export default "This is virtual!"';
+              }
+              return null; // other ids should be handled as usually
+            },
+          },
+        ],
+      },
+    },
   },
 });
