be061d57ebc45a51c018e21611fb91ede4fa4c8f
Pavlik Kiselev <pavlik.kiselev@ing.com>
2025-08-17 19:52:43 +0200

    fix: build and components/index page

--- src/utils/remark-plugings/copyMdjsStories/copyMdjsStories.mjs
+++ src/utils/remark-plugings/copyMdjsStories/copyMdjsStories.mjs
@@ -7,7 +7,7 @@ const NODE_MODULES_PATH = '/node_modules';
 const NODE_MODULES_LION_DOCS = '_lion_docs';
 const require = createRequire(import.meta.url);
 const DEBUG = 1;
-const DEBUG_COMPONENT = 'icon';
+const DEBUG_COMPONENT = 'overlays';
 const SHOW_MODULE_NOT_FOUND = false;
 
 const resolveLionImport = moduleResolvedPath => {
