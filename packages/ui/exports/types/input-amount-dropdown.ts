701fac53001c6f44ecf33d14fd35efe57d5ae73f
thatdudemanguy <robinvr1994@gmail.com>
2025-08-18 15:13:17 +0200

    chore(amountDropdown): export types.

---
+++ packages/ui/exports/types/input-amount-dropdown.ts
@@ -0,0 +1,9 @@
+export {
+  RegionMeta,
+  TemplateDataForDropdownInputAmount,
+  AmountDropdownModelValue,
+  CurrencyCode,
+  countryToCurrencyList,
+  RegionToCurrencyMap,
+  AllCurrenciesSet
+} from '../../components/input-amount-dropdown/types/index.js';
