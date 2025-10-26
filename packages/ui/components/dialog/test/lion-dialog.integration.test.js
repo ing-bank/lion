4b165b0ca32872f1647ba1089aada4a6377df1c8
Oleksii Kadurin <ovkadurin@gmail.com>
2025-10-26 13:03:47 +0100

    fix: nested overlays

--- packages/ui/components/dialog/test/lion-dialog.integration.test.js
+++ packages/ui/components/dialog/test/lion-dialog.integration.test.js
@@ -4,7 +4,6 @@ import { sendKeys } from '@web/test-runner-commands';
 
 import '@lion/ui/define/lion-button.js';
 import '@lion/ui/define/lion-combobox.js';
-import '@lion/ui/define/lion-select.js';
 import '@lion/ui/define/lion-select-rich.js';
 import '@lion/ui/define/lion-options.js';
 import '@lion/ui/define/lion-option.js';
@@ -18,24 +17,22 @@ import { mimicUserTyping } from '@lion/ui/combobox-test-helpers.js';
  */
 const fixture = /** @type {(arg: TemplateResult) => Promise<LionDialog>} */ (_fixture);
 
-const comboboxDataEntry = {
-  label: 'Apple',
-};
+const dropDownEntries = ['Apple', 'Banana'];
 
 describe('lion-dialog', () => {
   describe('lion-combobox integration', () => {
-    it('should close combobox dropdown on first Escape and should close the parent dialog on the second Escape', async () => {
+    it('should close lion-combobox dropdown on first Escape and should close the parent lion-dialog on the second Escape', async () => {
       const el = await fixture(html`
         <lion-dialog has-close-button>
           <lion-button slot="invoker">Open Dialog</lion-button>
           <div slot="header">Combobox example</div>
           <div slot="content">
             <lion-combobox name="combo" label="Select a fruit">
-              <lion-option .choiceValue="${comboboxDataEntry.label}"
-                >${comboboxDataEntry.label}</lion-option
-              >
+              ${dropDownEntries.map(
+                (/** @type { string } */ entry) =>
+                  html` <lion-option .choiceValue="${entry}">${entry}</lion-option>`,
+              )}
             </lion-combobox>
-            <p>The combobox will overflow outside of the dialog.</p>
           </div>
         </lion-dialog>
       `);
@@ -56,15 +53,58 @@ describe('lion-dialog', () => {
       await sendKeys({
         press: 'Escape',
       });
-      const isComboboxDropdownVisible = () =>
+      const isDropdownVisible = () =>
         combobox?.shadowRoot?.querySelector('dialog')?.checkVisibility();
       const isDialogVisible = () => el?.shadowRoot?.querySelector('dialog')?.checkVisibility();
-      expect(isComboboxDropdownVisible()).to.equal(false);
+      expect(isDropdownVisible()).to.equal(false);
       expect(isDialogVisible()).to.equal(true);
       await sendKeys({
         press: 'Escape',
       });
-      expect(isComboboxDropdownVisible()).to.equal(false);
+      expect(isDropdownVisible()).to.equal(false);
+      expect(isDialogVisible()).to.equal(false);
+    });
+  });
+
+  describe('lion-select-rich integration', () => {
+    it('should close lion-select-rich dropdown on first Escape and should close the parent lion-dialog on the second Escape', async () => {
+      const el = await fixture(html`
+        <lion-dialog has-close-button>
+          <lion-button slot="invoker">Open Dialog</lion-button>
+          <div slot="header">Combobox example</div>
+          <div slot="content">
+            <lion-select-rich label="Select a fruit">
+              ${dropDownEntries.map(
+                (/** @type { string } */ entry) =>
+                  html` <lion-option .choiceValue="${entry}">${entry}</lion-option>`,
+              )}
+            </lion-select-rich>
+          </div>
+        </lion-dialog>
+      `);
+
+      const dialogInvoker = /** @type {HTMLElement} */ (el.querySelector('[slot="invoker"]'));
+      dialogInvoker.click();
+      const selectRichInvoker = /** @type {HTMLElement} */ el.querySelector('lion-select-invoker');
+      const isSelectRichInvokerRendered = () => !!selectRichInvoker?.shadowRoot?.childNodes.length;
+      await waitUntil(isSelectRichInvokerRendered);
+      selectRichInvoker?.click();
+      const isDropdownVisible = () =>
+        el
+          ?.querySelector('lion-select-rich')
+          ?.shadowRoot?.querySelector('dialog')
+          ?.checkVisibility();
+      await waitUntil(isDropdownVisible);
+      await sendKeys({
+        press: 'Escape',
+      });
+      const isDialogVisible = () => el?.shadowRoot?.querySelector('dialog')?.checkVisibility();
+      expect(isDropdownVisible()).to.equal(false);
+      expect(isDialogVisible()).to.equal(true);
+      await sendKeys({
+        press: 'Escape',
+      });
+      expect(isDropdownVisible()).to.equal(false);
       expect(isDialogVisible()).to.equal(false);
     });
   });
