b43fc537e89b4567d9009a832db12f16b655f830
ayoayco <ayo@ayco.io>
2025-11-19 15:28:42 +0100

    chore(site): update content.config.ts

--- site/src/content.config.ts
+++ site/src/content.config.ts
@@ -2,23 +2,23 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const lionComponents = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './docs/components' }),
+  loader: glob({ pattern: '**/*.md', base: '../docs/components' }),
 });
 
 const lionFundamentals = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './docs/fundamentals' }),
+  loader: glob({ pattern: '**/*.md', base: '../docs/fundamentals' }),
 });
 
 const lionGuides = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './docs/guides' }),
+  loader: glob({ pattern: '**/*.md', base: '../docs/guides' }),
 });
 
 const lionBlog = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './docs/blog' }),
+  loader: glob({ pattern: '**/*.md', base: '../docs/blog' }),
 });
 
 const lionIndex = defineCollection({
-  loader: glob({ pattern: 'index.md', base: './docs' }),
+  loader: glob({ pattern: 'index.md', base: '../docs' }),
 });
 
 export const collections = { lionComponents, lionFundamentals, lionGuides, lionBlog, lionIndex };
