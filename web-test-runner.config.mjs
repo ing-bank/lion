3b0813a75d57ad85f6907a77d30ce01006120259
Thijs Louisse <Thijs.Louisse@ing.com>
2026-02-04 17:58:35 +0100

    chore: use native glob

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,18 +1,31 @@
 import { litSsrPlugin } from '@lit-labs/testing/web-test-runner-ssr-plugin.js';
+// @ts-expect-error
 import { playwrightLauncher } from '@web/test-runner-playwright';
-import { globby } from 'globby';
+import { glob } from 'node:fs/promises';
 
 const config = {
   shouldLoadPolyfill: !process.argv.includes('--no-scoped-registries-polyfill'),
   shouldRunDevMode: process.argv.includes('--dev-mode'),
 };
 
-const groups = (
-  await globby(['packages/*/test', 'packages/ui/components/**/test'], {
-    onlyDirectories: true,
-  })
-) // @ts-expect-error [update-es-version-later]
-  .map(dir => ({ name: dir.split('/').at(-2), files: `${dir}/**/*.test.js` })); // .filter(({name}) => name === 'overlays');
+async function getTestGroups() {
+  const allDirs = await Promise.all([
+    glob('packages/*/test'),
+    glob('packages/ui/components/**/test'),
+  ]).then(async iters => {
+    const all = [];
+    for (const iter of iters) for await (const d of iter) all.push(d);
+    return all;
+  });
+
+  return allDirs.map(dir => ({
+    // @ts-expect-error
+    name: dir.split('/').at(-2),
+    files: `${dir}/**/*.test.js`,
+  }));
+}
+
+const groups = await getTestGroups();
 
 /**
  * @type {import('@web/test-runner').TestRunnerConfig['testRunnerHtml']}
