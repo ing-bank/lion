a78de5ff5dad1a82e85e11d323d09db298c990d0
Knorgias <kostasnorgias@gmail.com>
2026-02-04 13:53:51 +0100

    test: add visual regression support to button component tests

--- packages/ui/components/button/test-suites/LionButtonSubmit.suite.js
+++ packages/ui/components/button/test-suites/LionButtonSubmit.suite.js
@@ -4,7 +4,7 @@ import { aTimeout, defineCE, expect, fixture, html, unsafeStatic } from '@open-w
 import { sendKeys } from '@web/test-runner-commands';
 import sinon from 'sinon';
 
-export function LionButtonSubmitSuite({ klass = LionButtonSubmit } = {}) {
+export function LionButtonSubmitSuite({ klass = LionButtonSubmit, visualDiffEnabled } = {}) {
   const tagStringButton = defineCE(class extends klass {});
   const tagButton = unsafeStatic(tagStringButton);
 
@@ -15,6 +15,9 @@ export function LionButtonSubmitSuite({ klass = LionButtonSubmit } = {}) {
       );
       expect(el.type).to.equal('submit');
       expect(el.getAttribute('type')).to.be.equal('submit');
+      if (visualDiffEnabled) {
+        await visualDiffEnabled(el, 'has-type-submit-by-default');
+      }
     });
 
     describe('Implicit form submission', () => {
