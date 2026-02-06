b491170c382e7db8bd97a7a4d3cd18fef59e31b1
Sebastian Kamiński <sebastian.kaminski978@gmail.com>
2026-02-07 00:36:34 +0100

    fix(OverlayController): improve hide behavior after teardown and registration check

--- packages/ui/components/overlays/src/OverlaysManager.js
+++ packages/ui/components/overlays/src/OverlaysManager.js
@@ -106,7 +106,7 @@ export class OverlaysManager {
    */
   hide(ctrlToHide) {
     if (!this.list.find(ctrl => ctrlToHide === ctrl)) {
-      return;
+      throw new Error('could not find controller to hide');
     }
     this.__shownList = this.shownList.filter(ctrl => ctrl !== ctrlToHide);
   }
