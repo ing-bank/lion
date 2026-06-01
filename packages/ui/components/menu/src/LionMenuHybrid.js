7686233549de7b881945269558db225f5c421df9
Oleksii Kadurin <ovkadurin@gmail.com>
2026-06-01 10:26:01 +0200

    revert changes

--- packages/ui/components/menu/src/LionMenuHybrid.js
+++ packages/ui/components/menu/src/LionMenuHybrid.js
@@ -21,9 +21,9 @@ export class LionMenuHybrid extends LionMenuOverlay {
     // Take inspiration from VisibilityToggleCtrl of portal elements
     /**
      * Terminology aligned with https://open-ui.org/components/openable.explainer/
-     * @type {'tabbable-disclosure'|'overlay'}
+     * @type {'disclosure'|'overlay'}
      */
-    this.openableMode = 'tabbable-disclosure';
+    this.openableMode = 'disclosure';
 
     this._shouldSetupOverlay = false;
   }
