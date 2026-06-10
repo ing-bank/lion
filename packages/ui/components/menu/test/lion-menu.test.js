467d688101205b9ef533244168c6eea56f46dbaa
Oleksii Kadurin <ovkadurin@gmail.com>
2026-06-10 10:59:50 +0200

    Chore/add tests for more button (#2773)

--- packages/ui/components/menu/test/lion-menu.test.js
+++ packages/ui/components/menu/test/lion-menu.test.js
@@ -1,4 +1,6 @@
 import { runInteractiveListMixinSuite } from '../test-suites/InteractiveListMixin.suite.js';
 import '@lion/ui/define/lion-menu.js';
+import { runLionMenuHybridSuite } from '../test-suites/LionMenuHybrid.suite.js';
 
 runInteractiveListMixinSuite({ tagString: 'lion-menu' });
+runLionMenuHybridSuite();
