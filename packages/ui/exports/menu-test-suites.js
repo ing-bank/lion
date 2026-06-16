fe5961df25a408281b67e21114094cb221332eba
gerjanvangeest <gerjan_van_geest@hotmail.com>
2026-06-16 17:01:06 +0200

    Revert "chore(menu): add more test suites"
    This reverts commit 47d5a9f3326d9d5589f1feb89ef4bcfd280f43ba.

--- packages/ui/exports/menu-test-suites.js
+++ packages/ui/exports/menu-test-suites.js
@@ -1,4 +1,2 @@
 /* eslint-disable import/no-relative-packages */
 export { runInteractiveListMixinSuite } from '../components/menu/test-suites/InteractiveListMixin.suite.js';
-export { runMultiLevelListMixinSuite } from '../components/menu/test-suites/MultiLevelListMixin.suite.js';
-export { runLionMenuHybridSuite } from '../components/menu/test-suites/LionMenuHybrid.suite.js';
