71acb3d55c8d4e98c500e10ecc8073cada2ad147
Lampros Varnavas <lampros.varnavas@ing.com>
2026-07-30 15:55:21 +0200

    feat: align validation messages (hu)

--- packages/ui/components/input-iban/translations/hu.js
+++ packages/ui/components/input-iban/translations/hu.js
@@ -2,34 +2,34 @@ export default {
   error: {
     IsIBAN: 'Kérjük, adjon meg érvényes {fieldName} értéket.',
     IsCountryIBAN:
-      'Kérjük, adjon meg érvényes {fieldName} értéket - {params, select,\n' +
-      'AT {Ausztria}\n' +
-      'BE {Belgium}\n' +
-      'CZ {Csehország}\n' +
-      'DE {Németország}\n' +
-      'ES {Spanyolország}\n' +
-      'FR {Franciaország}\n' +
-      'HU {Magyarország}\n' +
-      'IT {Olaszország}\n' +
-      'NL {Hollandia}\n' +
-      'PL {Lengyelország}\n' +
-      'RO {Románia}\n' +
+      'Kérjük, adjon meg érvényes {fieldName} értéket {params, select,\n' +
+      'AT {Ausztriából}\n' +
+      'BE {Belgiumból}\n' +
+      'CZ {Csehországból}\n' +
+      'DE {Németországból}\n' +
+      'ES {Spanyolországból}\n' +
+      'FR {Franciaországból}\n' +
+      'HU {Magyarországról}\n' +
+      'IT {Olaszországból}\n' +
+      'NL {Hollandiából}\n' +
+      'PL {Lengyelországból}\n' +
+      'RO {Romániából}\n' +
       'other {{params}}\n' +
       '}.',
     IsNotCountryIBAN:
-      '{fieldName} értéke - {userSuppliedCountryCode, select,\n' +
-      'AT {Ausztria}\n' +
-      'BE {Belgium}\n' +
-      'CZ {Csehország}\n' +
-      'DE {Németország}\n' +
-      'ES {Spanyolország}\n' +
-      'FR {Franciaország}\n' +
-      'HU {Magyarország}\n' +
-      'IT {Olaszország}\n' +
-      'NL {Hollandia}\n' +
-      'PL {Lengyelország}\n' +
-      'RO {Románia}\n' +
+      '{userSuppliedCountryCode, select,\n' +
+      'AT {Ausztriából}\n' +
+      'BE {Belgiumból}\n' +
+      'CZ {Csehországból}\n' +
+      'DE {Németországból}\n' +
+      'ES {Spanyolországból}\n' +
+      'FR {Franciaországból}\n' +
+      'HU {Magyarországról}\n' +
+      'IT {Olaszországból}\n' +
+      'NL {Hollandiából}\n' +
+      'PL {Lengyelországból}\n' +
+      'RO {Romániából}\n' +
       'other {{userSuppliedCountryCode}}\n' +
-      '} - nem engedélyezett.',
+      '} származó {fieldName} érték nem engedélyezett.',
   },
 };
