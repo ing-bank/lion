12b620fc319797816f4a84d812a819cf2caadd98
Oleksii Kadurin <ovkadurin@gmail.com>
2026-09-16 11:13:48 +0200

    chore: update tests for telephone component (#2854)

--- packages/ui/components/input-tel/test/LionInputTel.test.js
+++ packages/ui/components/input-tel/test/LionInputTel.test.js
@@ -1,3 +1,5 @@
 import { runInputTelSuite } from '@lion/ui/input-tel-test-suites.js';
+import { LionInputTel } from '@lion/ui/input-tel.js';
 
 runInputTelSuite();
+runInputTelSuite({ klass: LionInputTel, hasParentheses: true });
